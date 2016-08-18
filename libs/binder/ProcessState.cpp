6b335f5e14668a4c74471d0758a533209fb15c4c
openvela-robot <openvela-robot@xiaomi.com>
2016-08-18 18:23:22 +0800

    resolve merge conflicts of 5959d8e to nyc-dev-plus-aosp
    Change-Id: I1532b1e4ee225f821fcd6a9eaee11f20bac8c9e1

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -16,8 +16,6 @@
 
 #define LOG_TAG "ProcessState"
 
-#include <cutils/process_name.h>
-
 #include <binder/ProcessState.h>
 
 #include <utils/Atomic.h>
@@ -52,7 +50,7 @@ namespace android {
 class PoolThread : public Thread
 {
 public:
-    PoolThread(bool isMain)
+    explicit PoolThread(bool isMain)
         : mIsMain(isMain)
     {
     }
@@ -366,6 +364,13 @@ ProcessState::ProcessState()
 
 ProcessState::~ProcessState()
 {
+    if (mDriverFD >= 0) {
+        if (mVMStart != MAP_FAILED) {
+            munmap(mVMStart, BINDER_VM_SIZE);
+        }
+        close(mDriverFD);
+    }
+    mDriverFD = -1;
 }
         
 }; // namespace android

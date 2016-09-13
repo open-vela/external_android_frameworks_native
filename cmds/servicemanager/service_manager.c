218940626c36fe80655770169a7e6690b76d12d3
openvela-robot <openvela-robot@xiaomi.com>
2016-09-13 15:47:03 +0800

    ServiceManager: Restore basic uid check am: d149e66054
    Change-Id: I64b3db319f75d1f07e8439e393e5c3c8d5b192e2

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -8,8 +8,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include <cutils/multiuser.h>
-
 #include <private/android_filesystem_config.h>
 
 #include <selinux/android.h>
@@ -124,7 +122,7 @@ static int svc_can_register(const uint16_t *name, size_t name_len, pid_t spid, u
 {
     const char *perm = "add";
 
-    if (multiuser_get_app_id(uid) >= AID_APP) {
+    if (uid >= AID_APP) {
         return 0; /* Don't allow apps to register services */
     }
 

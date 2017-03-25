ef0cf9ad0bb59b26d0bcac25994619f10db0afa1
openvela-robot <openvela-robot@xiaomi.com>
2017-03-25 17:12:59 +0800

    This test is used to check if the kernel scheduler
    1. supports priority inheritance.
    2. utilize the sync flag.
    
    It also make statistic on average, best and worst latency
    as well as number of deadline miss with a configurable
    deadline specified in us.
    
    The output is in json format hopefully to make it easy
    to integrated with present test framework.
    
    usage:
    
    ./schd-dbg                   # test and output json
    ./schd-dbg -i 1              # number of iterations
    ./schd-dbg -pair 4           # number of process pairs
    ./schd-dbg -deadline_us 2500 # deadline in us
    ./schd-dbg -v                # debug
    
    Test: pixel
    Change-Id: Idccf0d402dcd7b81c639020364b5ee32075e18cb

--- cmds/servicemanager/service_manager.c
+++ cmds/servicemanager/service_manager.c
@@ -17,12 +17,13 @@
 
 #include "binder.h"
 
-#ifdef VENDORSERVICEMANAGER
-#define LOG_TAG "VendorServiceManager"
+#if 0
+#define ALOGI(x...) fprintf(stderr, "svcmgr: " x)
+#define ALOGE(x...) fprintf(stderr, "svcmgr: " x)
 #else
 #define LOG_TAG "ServiceManager"
-#endif
 #include <log/log.h>
+#endif
 
 struct audit_data {
     pid_t pid;
@@ -359,28 +360,14 @@ static int audit_callback(void *data, __unused security_class_t cls, char *buf, 
     return 0;
 }
 
-int main(int argc, char** argv)
+int main()
 {
     struct binder_state *bs;
     union selinux_callback cb;
-    char *driver;
 
-    if (argc > 1) {
-        driver = argv[1];
-    } else {
-        driver = "/dev/binder";
-    }
-
-    bs = binder_open(driver, 128*1024);
+    bs = binder_open(128*1024);
     if (!bs) {
-#ifdef VENDORSERVICEMANAGER
-        ALOGW("failed to open binder driver %s\n", driver);
-        while (true) {
-            sleep(UINT_MAX);
-        }
-#else
-        ALOGE("failed to open binder driver %s\n", driver);
-#endif
+        ALOGE("failed to open binder driver\n");
         return -1;
     }
 
@@ -394,11 +381,7 @@ int main(int argc, char** argv)
     cb.func_log = selinux_log_callback;
     selinux_set_callback(SELINUX_CB_LOG, cb);
 
-#ifdef VENDORSERVICEMANAGER
-    sehandle = selinux_android_vendor_service_context_handle();
-#else
     sehandle = selinux_android_service_context_handle();
-#endif
     selinux_status_open(true);
 
     if (sehandle == NULL) {

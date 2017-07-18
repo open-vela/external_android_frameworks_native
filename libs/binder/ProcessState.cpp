a1d5003c00e3beb14ddb57ed93ad5034b1169b3b
openvela-robot <openvela-robot@xiaomi.com>
2017-07-18 08:55:28 +0800

    Merge "Fix clang static anaylzer warnings."

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -71,22 +71,13 @@ sp<ProcessState> ProcessState::self()
     if (gProcess != NULL) {
         return gProcess;
     }
-    gProcess = new ProcessState("/dev/binder");
+    gProcess = new ProcessState;
     return gProcess;
 }
 
-sp<ProcessState> ProcessState::initWithDriver(const char* driver)
+sp<ProcessState> ProcessState::selfOrNull()
 {
     Mutex::Autolock _l(gProcessMutex);
-    if (gProcess != NULL) {
-        // Allow for initWithDriver to be called repeatedly with the same
-        // driver.
-        if (!strcmp(gProcess->getDriverName().c_str(), driver)) {
-            return gProcess;
-        }
-        LOG_ALWAYS_FATAL("ProcessState was already initialized.");
-    }
-    gProcess = new ProcessState(driver);
     return gProcess;
 }
 
@@ -174,6 +165,46 @@ bool ProcessState::becomeContextManager(context_check_func checkFunc, void* user
         }
     }
     return mManagesContexts;
+}
+
+// Get references to userspace objects held by the kernel binder driver
+// Writes up to count elements into buf, and returns the total number
+// of references the kernel has, which may be larger than count.
+// buf may be NULL if count is 0.  The pointers returned by this method
+// should only be used for debugging and not dereferenced, they may
+// already be invalid.
+ssize_t ProcessState::getKernelReferences(size_t buf_count, uintptr_t* buf)
+{
+    // TODO: remove these when they are defined by bionic's binder.h
+    struct binder_node_debug_info {
+        binder_uintptr_t ptr;
+        binder_uintptr_t cookie;
+        __u32 has_strong_ref;
+        __u32 has_weak_ref;
+    };
+#define BINDER_GET_NODE_DEBUG_INFO _IOWR('b', 11, struct binder_node_debug_info)
+
+    binder_node_debug_info info = {};
+
+    uintptr_t* end = buf ? buf + buf_count : NULL;
+    size_t count = 0;
+
+    do {
+        status_t result = ioctl(mDriverFD, BINDER_GET_NODE_DEBUG_INFO, &info);
+        if (result < 0) {
+            return -1;
+        }
+        if (info.ptr != 0) {
+            if (buf && buf < end)
+                *buf++ = info.ptr;
+            count++;
+            if (buf && buf < end)
+                *buf++ = info.cookie;
+            count++;
+        }
+    } while (info.ptr != 0);
+
+    return count;
 }
 
 ProcessState::handle_entry* ProcessState::lookupHandleLocked(int32_t handle)
@@ -322,13 +353,9 @@ void ProcessState::giveThreadPoolName() {
     androidSetThreadName( makeBinderThreadName().string() );
 }
 
-String8 ProcessState::getDriverName() {
-    return mDriverName;
-}
-
-static int open_driver(const char *driver)
+static int open_driver()
 {
-    int fd = open(driver, O_RDWR | O_CLOEXEC);
+    int fd = open("/dev/binder", O_RDWR | O_CLOEXEC);
     if (fd >= 0) {
         int vers = 0;
         status_t result = ioctl(fd, BINDER_VERSION, &vers);
@@ -338,8 +365,7 @@ static int open_driver(const char *driver)
             fd = -1;
         }
         if (result != 0 || vers != BINDER_CURRENT_PROTOCOL_VERSION) {
-          ALOGE("Binder driver protocol(%d) does not match user space protocol(%d)! ioctl() return value: %d",
-                vers, BINDER_CURRENT_PROTOCOL_VERSION, result);
+            ALOGE("Binder driver protocol does not match user space protocol!");
             close(fd);
             fd = -1;
         }
@@ -349,14 +375,13 @@ static int open_driver(const char *driver)
             ALOGE("Binder ioctl to set max threads failed: %s", strerror(errno));
         }
     } else {
-        ALOGW("Opening '%s' failed: %s\n", driver, strerror(errno));
+        ALOGW("Opening '/dev/binder' failed: %s\n", strerror(errno));
     }
     return fd;
 }
 
-ProcessState::ProcessState(const char *driver)
-    : mDriverName(String8(driver))
-    , mDriverFD(open_driver(driver))
+ProcessState::ProcessState()
+    : mDriverFD(open_driver())
     , mVMStart(MAP_FAILED)
     , mThreadCountLock(PTHREAD_MUTEX_INITIALIZER)
     , mThreadCountDecrement(PTHREAD_COND_INITIALIZER)
@@ -377,7 +402,6 @@ ProcessState::ProcessState(const char *driver)
             ALOGE("Using /dev/binder failed: unable to mmap transaction memory.\n");
             close(mDriverFD);
             mDriverFD = -1;
-            mDriverName.clear();
         }
     }
 
--- libs/binder/include/binder/ProcessState.h
+++ libs/binder/include/binder/ProcessState.h
@@ -35,11 +35,7 @@ class ProcessState : public virtual RefBase
 {
 public:
     static  sp<ProcessState>    self();
-    /* initWithDriver() can be used to configure libbinder to use
-     * a different binder driver dev node. It must be called *before*
-     * any call to ProcessState::self(). /dev/binder remains the default.
-     */
-    static  sp<ProcessState>    initWithDriver(const char *driver);
+    static  sp<ProcessState>    selfOrNull();
 
             void                setContextObject(const sp<IBinder>& object);
             sp<IBinder>         getContextObject(const sp<IBinder>& caller);
@@ -69,12 +65,12 @@ public:
             status_t            setThreadPoolMaxThreadCount(size_t maxThreads);
             void                giveThreadPoolName();
 
-            String8             getDriverName();
+            ssize_t             getKernelReferences(size_t count, uintptr_t* buf);
 
 private:
     friend class IPCThreadState;
     
-                                ProcessState(const char* driver);
+                                ProcessState();
                                 ~ProcessState();
 
                                 ProcessState(const ProcessState& o);
@@ -88,7 +84,6 @@ private:
 
             handle_entry*       lookupHandleLocked(int32_t handle);
 
-            String8             mDriverName;
             int                 mDriverFD;
             void*               mVMStart;
 

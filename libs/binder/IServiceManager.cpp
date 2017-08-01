057360c6c272560a6f56c72901107ad8a5facce3
openvela-robot <openvela-robot@xiaomi.com>
2017-08-01 17:09:07 +0800

    Use cutils/android_filesystem_config.h
    private/android_filesystem_config.h is not available for
    vndservicemanager.
    
    Test: m -j
    Test: BOARD_VNDK_VERSION m -j
    
    Bug: 63135587
    Change-Id: Iaf9911a519ebbb798184e93b2911d0e813e6026c
    Merged-In: Iaf9911a519ebbb798184e93b2911d0e813e6026c

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -23,7 +23,6 @@
 #include <binder/Parcel.h>
 #include <utils/String8.h>
 #include <utils/SystemClock.h>
-#include <utils/CallStack.h>
 
 #include <private/binder/Static.h>
 
@@ -137,12 +136,7 @@ public:
         unsigned n;
         for (n = 0; n < 5; n++){
             if (n > 0) {
-                if (!strcmp(ProcessState::self()->getDriverName().c_str(), "/dev/vndbinder")) {
-                    ALOGI("Waiting for vendor service %s...", String8(name).string());
-                    CallStack stack(LOG_TAG);
-                } else {
-                    ALOGI("Waiting for service %s...", String8(name).string());
-                }
+                ALOGI("Waiting for service %s...", String8(name).string());
                 sleep(1);
             }
             sp<IBinder> svc = checkService(name);

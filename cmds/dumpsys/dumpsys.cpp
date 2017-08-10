388732c40e7c5dfe836d5c31d140b3d46a025a5c
openvela-robot <openvela-robot@xiaomi.com>
2017-08-10 09:33:56 +0800

    Cast binder data type to uint64_t in high bits test am: 583e896cdf  -s ours
    Change-Id: I0c4a0950e1960afc5ee5f6797bc70ff88a18b5db

--- cmds/dumpsys/dumpsys.cpp
+++ cmds/dumpsys/dumpsys.cpp
@@ -176,7 +176,7 @@ int Dumpsys::main(int argc, char* const argv[]) {
     }
 
     for (size_t i = 0; i < N; i++) {
-        const String16& service_name = std::move(services[i]);
+        String16 service_name = std::move(services[i]);
         if (IsSkipped(skippedServices, service_name)) continue;
 
         sp<IBinder> service = sm_->checkService(service_name);

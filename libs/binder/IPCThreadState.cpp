afeb0422196636d044a46a225e7405a78c443a0f
openvela-robot <openvela-robot@xiaomi.com>
2017-09-20 16:21:11 +0800

    Merge "packagemanager_native access to package installer, version" into oc-mr1-dev am: f14e953f31
    Change-Id: I9fa9b781b8430b9aa646b51c76b4166f2ee2a824

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -686,7 +686,7 @@ void IPCThreadState::expungeHandle(int32_t handle, IBinder* binder)
 #if LOG_REFCOUNTS
     ALOGV("IPCThreadState::expungeHandle(%ld)\n", handle);
 #endif
-    self()->mProcess->expungeHandle(handle, binder); // NOLINT
+    self()->mProcess->expungeHandle(handle, binder);
 }
 
 status_t IPCThreadState::requestDeathNotification(int32_t handle, BpBinder* proxy)

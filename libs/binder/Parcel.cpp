7a0743585d741823e1eed5eb17fbefc4e0b323ef
openvela-robot <openvela-robot@xiaomi.com>
2017-10-04 03:49:47 +0800

    Merge "Added PriorityDump helper util"

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -2645,7 +2645,7 @@ status_t Parcel::continueWrite(size_t desired)
                 pthread_mutex_unlock(&gParcelGlobalAllocSizeLock);
                 mData = data;
                 mDataCapacity = desired;
-            } else {
+            } else if (desired > mDataCapacity) {
                 mError = NO_MEMORY;
                 return NO_MEMORY;
             }

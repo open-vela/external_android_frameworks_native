9d56f4dd84744b29a0e2967b30df7039e299ce53
liujinye <liujinye@xiaomi.com>
2018-05-11 09:53:53 +0800

    Merge changes from topic "am-27943b02-3710-49e4-84f6-eabc78fdbe01" into nyc-dr1-dev
    * changes:
      [automerger] Don't pad before calling writeInPlace(). am: 606121f264 am: 21ce4741e6 am: 3842e33471 am: 9fe2134fc8 am: 228722a101
      [automerger] Don't pad before calling writeInPlace(). am: 606121f264 am: 21ce4741e6 am: 3842e33471 am: 9fe2134fc8
      [automerger] Don't pad before calling writeInPlace(). am: 606121f264 am: 21ce4741e6 am: 3842e33471
      [automerger] Don't pad before calling writeInPlace(). am: 606121f264 am: 21ce4741e6
      [automerger] Don't pad before calling writeInPlace(). am: 606121f264
      Don't pad before calling writeInPlace().

--- libs/binder/Static.cpp
+++ libs/binder/Static.cpp
@@ -48,7 +48,7 @@ protected:
 class FdTextOutput : public BufferedTextOutput
 {
 public:
-    explicit FdTextOutput(int fd) : BufferedTextOutput(MULTITHREADED), mFD(fd) { }
+    FdTextOutput(int fd) : BufferedTextOutput(MULTITHREADED), mFD(fd) { }
     virtual ~FdTextOutput() { };
 
 protected:
@@ -94,9 +94,6 @@ static LibBinderIPCtStatics gIPCStatics;
 
 Mutex gDefaultServiceManagerLock;
 sp<IServiceManager> gDefaultServiceManager;
-#ifndef __ANDROID_VNDK__
 sp<IPermissionController> gPermissionController;
-#endif
-bool gSystemBootCompleted = false;
 
 }   // namespace android

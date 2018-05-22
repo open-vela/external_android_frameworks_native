cb92ee24bf4ad38e3aebae28a87a5b35302c01c9
openvela-robot <openvela-robot@xiaomi.com>
2018-05-22 12:04:23 +0800

    Merge "Remove thread shutdown logic." am: 5730cc10c7 am: c1b91a4460
    Change-Id: I124a001cde37f9c3bd8cdfc4a59ccdc250fd24e5

--- libs/binder/Static.cpp
+++ libs/binder/Static.cpp
@@ -79,6 +79,9 @@ sp<ProcessState> gProcess;
 
 Mutex gDefaultServiceManagerLock;
 sp<IServiceManager> gDefaultServiceManager;
+#ifndef __ANDROID_VNDK__
 sp<IPermissionController> gPermissionController;
+#endif
+bool gSystemBootCompleted = false;
 
 }   // namespace android

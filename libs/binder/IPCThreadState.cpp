c0c764a18cba37adbf3999b86b7d907014879238
openvela-robot <openvela-robot@xiaomi.com>
2018-05-29 12:25:45 +0800

    lshal: Released column is now Y/N/? am: b7caaff777
    Change-Id: If9adc03c66e94ab3f775d11b09bcba659655ef7c

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -409,15 +409,6 @@ void IPCThreadState::flushCommands()
     if (mProcess->mDriverFD <= 0)
         return;
     talkWithDriver(false);
-    // The flush could have caused post-write refcount decrements to have
-    // been executed, which in turn could result in BC_RELEASE/BC_DECREFS
-    // being queued in mOut. So flush again, if we need to.
-    if (mOut.dataSize() > 0) {
-        talkWithDriver(false);
-    }
-    if (mOut.dataSize() > 0) {
-        ALOGW("mOut.dataSize() > 0 after flushCommands()");
-    }
 }
 
 void IPCThreadState::blockUntilThreadAvailable()
@@ -508,21 +499,6 @@ void IPCThreadState::processPendingDerefs()
             }
         }
     }
-}
-
-void IPCThreadState::processPostWriteDerefs()
-{
-    for (size_t i = 0; i < mPostWriteWeakDerefs.size(); i++) {
-        RefBase::weakref_type* refs = mPostWriteWeakDerefs[i];
-        refs->decWeak(mProcess.get());
-    }
-    mPostWriteWeakDerefs.clear();
-
-    for (size_t i = 0; i < mPostWriteStrongDerefs.size(); i++) {
-        RefBase* obj = mPostWriteStrongDerefs[i];
-        obj->decStrong(mProcess.get());
-    }
-    mPostWriteStrongDerefs.clear();
 }
 
 void IPCThreadState::joinThreadPool(bool isMain)
@@ -651,14 +627,11 @@ status_t IPCThreadState::transact(int32_t handle,
     return err;
 }
 
-void IPCThreadState::incStrongHandle(int32_t handle, BpBinder *proxy)
+void IPCThreadState::incStrongHandle(int32_t handle)
 {
     LOG_REMOTEREFS("IPCThreadState::incStrongHandle(%d)\n", handle);
     mOut.writeInt32(BC_ACQUIRE);
     mOut.writeInt32(handle);
-    // Create a temp reference until the driver has handled this command.
-    proxy->incStrong(mProcess.get());
-    mPostWriteStrongDerefs.push(proxy);
 }
 
 void IPCThreadState::decStrongHandle(int32_t handle)
@@ -668,14 +641,11 @@ void IPCThreadState::decStrongHandle(int32_t handle)
     mOut.writeInt32(handle);
 }
 
-void IPCThreadState::incWeakHandle(int32_t handle, BpBinder *proxy)
+void IPCThreadState::incWeakHandle(int32_t handle)
 {
     LOG_REMOTEREFS("IPCThreadState::incWeakHandle(%d)\n", handle);
     mOut.writeInt32(BC_INCREFS);
     mOut.writeInt32(handle);
-    // Create a temp reference until the driver has handled this command.
-    proxy->getWeakRefs()->incWeak(mProcess.get());
-    mPostWriteWeakDerefs.push(proxy->getWeakRefs());
 }
 
 void IPCThreadState::decWeakHandle(int32_t handle)
@@ -927,10 +897,8 @@ status_t IPCThreadState::talkWithDriver(bool doReceive)
         if (bwr.write_consumed > 0) {
             if (bwr.write_consumed < mOut.dataSize())
                 mOut.remove(0, bwr.write_consumed);
-            else {
+            else
                 mOut.setDataSize(0);
-                processPostWriteDerefs();
-            }
         }
         if (bwr.read_consumed > 0) {
             mIn.setDataSize(bwr.read_consumed);

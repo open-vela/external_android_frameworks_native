117fd9bb81df0dd6c5863e5ece892c8ca301a5d6
openvela-robot <openvela-robot@xiaomi.com>
2018-08-08 02:35:55 +0800

    Merge "Merge Android Pie into master" am: 25776b92a2 am: 2bfa9353e0 am: ca597b4d77 am: 5b64d5d821
    Change-Id: If96a2a2b320101b040d59866b9fdedd9d6f412d3

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -27,6 +27,7 @@
 #include <cutils/properties.h>
 #include <utils/String8.h>
 #include <utils/SystemClock.h>
+#include <utils/CallStack.h>
 
 #include <private/binder/Static.h>
 
@@ -160,8 +161,12 @@ public:
         int n = 0;
         while (uptimeMillis() < timeout) {
             n++;
-            ALOGI("Waiting for service '%s' on '%s'...", String8(name).string(),
-                ProcessState::self()->getDriverName().c_str());
+            if (isVendorService) {
+                ALOGI("Waiting for vendor service %s...", String8(name).string());
+                CallStack stack(LOG_TAG);
+            } else if (n%10 == 0) {
+                ALOGI("Waiting for service %s...", String8(name).string());
+            }
             usleep(1000*sleepTime);
 
             sp<IBinder> svc = checkService(name);

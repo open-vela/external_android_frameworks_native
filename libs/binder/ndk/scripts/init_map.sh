littleGnAl/mirror_platform_frameworks_native

libbinder_ndk: to/fromJavaBinder
This allows for a conversion between android.os.IBinder and AIBinder.

Bug: 111445392
Test: wip
Change-Id: Icd3949d298bf602f7d24dc5ec92048df902095cb

libs/binder/ndk/scripts/init_map.sh
@@ -6,6 +6,7 @@ echo "LIBBINDER_NDK { # introduced=29"
 echo "  global:"
 {
     grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_ibinder.h;
+    grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_ibinder_ndk.h;
     grep -oP "AParcel_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_parcel.h;
     grep -oP "AStatus_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_status.h;
 } | sort | uniq | awk '{ print "    " $0 ";"; }'

open-vela/external_android_frameworks_native

Test: android.binder.cts

libs/binder/ndk/scripts/init_map.sh
@@ -6,6 +6,7 @@ echo "LIBBINDER_NDK { # introduced=29"
 echo "  global:"
 {
     grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_ibinder.h;
+    grep -oP "AIBinder_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_ibinder_jni.h;
     grep -oP "AParcel_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_parcel.h;
     grep -oP "AStatus_[a-zA-Z0-9_]+(?=\()" include_ndk/android/binder_status.h;
 } | sort | uniq | awk '{ print "    " $0 ";"; }'
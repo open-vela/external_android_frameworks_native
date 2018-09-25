007653f93eb06afe1dc99463cb44ce6e2b022593
openvela-robot <openvela-robot@xiaomi.com>
2018-09-25 15:00:40 +0800

    Merge "libbinder: Status writeToParcel no OK on ERR" am: b6044961ea am: 44a045d958
    Change-Id: I032fdd6e4a8827a80d3d8973b486a2bf94348c41

--- libs/binder/ndk/test/main_client.cpp
+++ libs/binder/ndk/test/main_client.cpp
@@ -15,7 +15,6 @@
  */
 
 #include <android-base/logging.h>
-#include <android/binder_ibinder_jni.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
 #include <gtest/gtest.h>
@@ -101,11 +100,6 @@ TEST(NdkBinder, EqualityOfRemoteBinderPointer) {
 
     AIBinder_decStrong(binderA);
     AIBinder_decStrong(binderB);
-}
-
-TEST(NdkBinder, ToFromJavaNullptr) {
-    EXPECT_EQ(nullptr, AIBinder_toJavaBinder(nullptr, nullptr));
-    EXPECT_EQ(nullptr, AIBinder_fromJavaBinder(nullptr, nullptr));
 }
 
 TEST(NdkBinder, ABpBinderRefCount) {

24931833d0143b6aa949f78f28781fc084095ffc
openvela-robot <openvela-robot@xiaomi.com>
2018-09-25 21:16:55 +0800

    Merge "libbinder: Status writeToParcel no OK on ERR"

--- libs/binder/ndk/include_ndk/android/binder_status.h
+++ libs/binder/ndk/include_ndk/android/binder_status.h
@@ -30,7 +30,6 @@
 #include <sys/cdefs.h>
 
 __BEGIN_DECLS
-#if __ANDROID_API__ >= __ANDROID_API_Q__
 
 enum {
     STATUS_OK = 0,
@@ -103,19 +102,19 @@ typedef struct AStatus AStatus;
 /**
  * New status which is considered a success.
  */
-__attribute__((warn_unused_result)) AStatus* AStatus_newOk() __INTRODUCED_IN(29);
+__attribute__((warn_unused_result)) AStatus* AStatus_newOk();
 
 /**
  * New status with exception code.
  */
-__attribute__((warn_unused_result)) AStatus* AStatus_fromExceptionCode(binder_exception_t exception)
-        __INTRODUCED_IN(29);
+__attribute__((warn_unused_result)) AStatus* AStatus_fromExceptionCode(
+        binder_exception_t exception);
 
 /**
  * New status with exception code and message.
  */
 __attribute__((warn_unused_result)) AStatus* AStatus_fromExceptionCodeWithMessage(
-        binder_exception_t exception, const char* message) __INTRODUCED_IN(29);
+        binder_exception_t exception, const char* message);
 
 /**
  * New status with a service speciic error.
@@ -123,7 +122,7 @@ __attribute__((warn_unused_result)) AStatus* AStatus_fromExceptionCodeWithMessag
  * This is considered to be EX_TRANSACTION_FAILED with extra information.
  */
 __attribute__((warn_unused_result)) AStatus* AStatus_fromServiceSpecificError(
-        int32_t serviceSpecific) __INTRODUCED_IN(29);
+        int32_t serviceSpecific);
 
 /**
  * New status with a service specific error and message.
@@ -131,26 +130,25 @@ __attribute__((warn_unused_result)) AStatus* AStatus_fromServiceSpecificError(
  * This is considered to be EX_TRANSACTION_FAILED with extra information.
  */
 __attribute__((warn_unused_result)) AStatus* AStatus_fromServiceSpecificErrorWithMessage(
-        int32_t serviceSpecific, const char* message) __INTRODUCED_IN(29);
+        int32_t serviceSpecific, const char* message);
 
 /**
  * New status with binder_status_t. This is typically for low level failures when a binder_status_t
  * is returned by an API on AIBinder or AParcel, and that is to be returned from a method returning
  * an AStatus instance.
  */
-__attribute__((warn_unused_result)) AStatus* AStatus_fromStatus(binder_status_t status)
-        __INTRODUCED_IN(29);
+__attribute__((warn_unused_result)) AStatus* AStatus_fromStatus(binder_status_t status);
 
 /**
  * Whether this object represents a successful transaction. If this function returns true, then
  * AStatus_getExceptionCode will return EX_NONE.
  */
-bool AStatus_isOk(const AStatus* status) __INTRODUCED_IN(29);
+bool AStatus_isOk(const AStatus* status);
 
 /**
  * The exception that this status object represents.
  */
-binder_exception_t AStatus_getExceptionCode(const AStatus* status) __INTRODUCED_IN(29);
+binder_exception_t AStatus_getExceptionCode(const AStatus* status);
 
 /**
  * The service specific error if this object represents one. This function will only ever return a
@@ -158,7 +156,7 @@ binder_exception_t AStatus_getExceptionCode(const AStatus* status) __INTRODUCED_
  * 0, the status object may still represent a different exception or status. To find out if this
  * transaction as a whole is okay, use AStatus_isOk instead.
  */
-int32_t AStatus_getServiceSpecificError(const AStatus* status) __INTRODUCED_IN(29);
+int32_t AStatus_getServiceSpecificError(const AStatus* status);
 
 /**
  * The status if this object represents one. This function will only ever return a non-zero result
@@ -166,7 +164,7 @@ int32_t AStatus_getServiceSpecificError(const AStatus* status) __INTRODUCED_IN(2
  * object may represent a different exception or a service specific error. To find out if this
  * transaction as a whole is okay, use AStatus_isOk instead.
  */
-binder_status_t AStatus_getStatus(const AStatus* status) __INTRODUCED_IN(29);
+binder_status_t AStatus_getStatus(const AStatus* status);
 
 /**
  * If there is a message associated with this status, this will return that message. If there is no
@@ -174,14 +172,13 @@ binder_status_t AStatus_getStatus(const AStatus* status) __INTRODUCED_IN(29);
  *
  * The returned string has the lifetime of the status object passed into this function.
  */
-const char* AStatus_getMessage(const AStatus* status) __INTRODUCED_IN(29);
+const char* AStatus_getMessage(const AStatus* status);
 
 /**
  * Deletes memory associated with the status instance.
  */
-void AStatus_delete(AStatus* status) __INTRODUCED_IN(29);
+void AStatus_delete(AStatus* status);
 
-#endif //__ANDROID_API__ >= __ANDROID_API_Q__
 __END_DECLS
 
 /** @} */

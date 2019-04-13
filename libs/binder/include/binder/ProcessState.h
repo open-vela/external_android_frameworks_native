2229b7190c494f64923baddf638b6ddd79aafac0
openvela-robot <openvela-robot@xiaomi.com>
2019-04-13 23:16:05 +0800

    Snap for 5467653 from ab949f66aebe9016ffa6624d10387a6cb9caef83 to rvc-release
    Change-Id: Ia8e5b2ef888dd214af201d7437724d2504cd0fa9

--- libs/binder/include/binder/ProcessState.h
+++ libs/binder/include/binder/ProcessState.h
@@ -124,8 +124,6 @@ private:
             int64_t             mStarvationStartTimeMs;
 
     mutable Mutex               mLock;  // protects everything below.
-            // TODO: mManagesContexts is often accessed without the lock.
-            //       Explain why that's safe.
 
             Vector<handle_entry>mHandleToObject;
 

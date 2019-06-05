f58206d07ad8dac19c31eb20665f3b5f1d55f90a
openvela-robot <openvela-robot@xiaomi.com>
2019-06-05 10:10:38 +0800

    [automerger skipped] Merge "[RESTRICT AUTOMERGE] libbinder: Status: check dataPosition sets." into oc-dev am: 070703d59f am: 035de66202 am: ac42e38c05 -s ours am skip reason: subject contains skip directive
    Change-Id: I7f3ed68c60f6e801b252593242f864c03b7c13af

--- libs/binder/include/binder/TextOutput.h
+++ libs/binder/include/binder/TextOutput.h
@@ -38,7 +38,7 @@ public:
     
     class Bundle {
     public:
-        inline explicit Bundle(TextOutput& to) : mTO(to) { to.pushBundle(); }
+        inline Bundle(TextOutput& to) : mTO(to) { to.pushBundle(); }
         inline ~Bundle() { mTO.popBundle(); }
     private:
         TextOutput&     mTO;
@@ -80,7 +80,7 @@ TextOutput& operator<<(TextOutput& to, TextOutputManipFunc func);
 class TypeCode
 {
 public:
-    inline explicit TypeCode(uint32_t code);
+    inline TypeCode(uint32_t code);
     inline ~TypeCode();
 
     inline uint32_t typeCode() const;

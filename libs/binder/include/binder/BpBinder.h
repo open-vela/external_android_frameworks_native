6e6be81952a71b07c6514c50bcc3fd1abeee85d1
openvela-robot <openvela-robot@xiaomi.com>
2019-06-06 23:19:33 +0800

    Merge "Free mObjects if no objects left to realloc on resize" into qt-dev

--- libs/binder/include/binder/BpBinder.h
+++ libs/binder/include/binder/BpBinder.h
@@ -67,6 +67,7 @@ public:
 
     virtual BpBinder*   remoteBinder();
 
+            status_t    setConstantData(const void* data, size_t size);
             void        sendObituary();
 
     static uint32_t     getBinderProxyCount(uint32_t uid);

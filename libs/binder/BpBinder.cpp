e6d024b6dfc410ed2448adb933aed38e704ca733
liujinye <liujinye@xiaomi.com>
2019-07-12 13:50:18 +0800

    Merge "libbinder: remove unused deps" am: f7e9de6177 am: 62ab5af58b am: 1d727767b2 am: 6b856984b8
    Change-Id: Ibb6bb52a271d631a7078bae1d7e49a9e0215cc85

--- libs/binder/BpBinder.cpp
+++ libs/binder/BpBinder.cpp
@@ -186,7 +186,10 @@ status_t BpBinder::pingBinder()
 {
     Parcel send;
     Parcel reply;
-    return transact(PING_TRANSACTION, send, &reply);
+    status_t err = transact(PING_TRANSACTION, send, &reply);
+    if (err != NO_ERROR) return err;
+    if (reply.dataSize() < sizeof(status_t)) return NOT_ENOUGH_DATA;
+    return (status_t)reply.readInt32();
 }
 
 status_t BpBinder::dump(int fd, const Vector<String16>& args)

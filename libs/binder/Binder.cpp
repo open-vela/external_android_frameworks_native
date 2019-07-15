3d0c92b8c6b7ebc3b78ef777c22808c221c09523
openvela-robot <openvela-robot@xiaomi.com>
2019-07-16 00:10:30 +0800

    Merge "libbinder: empty ping transaction"

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -144,7 +144,7 @@ status_t BBinder::transact(
     status_t err = NO_ERROR;
     switch (code) {
         case PING_TRANSACTION:
-            reply->writeInt32(pingBinder());
+            err = pingBinder();
             break;
         default:
             err = onTransact(code, data, reply, flags);
@@ -234,7 +234,7 @@ void BBinder::setRequestingSid(bool requestingSid)
         if (!e) return; // out of memory
     }
 
-    e->mRequestingSid = requestingSid;
+    e->mRequestingSid = true;
 }
 
 BBinder::~BBinder()

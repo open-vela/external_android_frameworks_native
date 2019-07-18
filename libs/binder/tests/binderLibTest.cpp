429f52d69df75383bd021cbe6a71b517fa98f737
liujinye <liujinye@xiaomi.com>
2019-07-18 21:22:44 +0800

    Merge "libbinder: tests w/o '-Wno-*'"

--- libs/binder/tests/binderLibTest.cpp
+++ libs/binder/tests/binderLibTest.cpp
@@ -71,8 +71,6 @@ enum BinderLibTestTranscationCode {
     BINDER_LIB_TEST_DELAYED_EXIT_TRANSACTION,
     BINDER_LIB_TEST_GET_PTR_SIZE_TRANSACTION,
     BINDER_LIB_TEST_CREATE_BINDER_TRANSACTION,
-    BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION,
-    BINDER_LIB_TEST_ECHO_VECTOR,
 };
 
 pid_t start_server_process(int arg2, bool usePoll = false)
@@ -181,7 +179,6 @@ class BinderLibTest : public ::testing::Test {
     public:
         virtual void SetUp() {
             m_server = static_cast<BinderLibTestEnv *>(binder_env)->getServer();
-            IPCThreadState::self()->restoreCallingWorkSource(0); 
         }
         virtual void TearDown() {
         }
@@ -897,132 +894,6 @@ TEST_F(BinderLibTest, OnewayQueueing)
     EXPECT_EQ(NO_ERROR, ret);
 }
 
-TEST_F(BinderLibTest, WorkSourceUnsetByDefault)
-{
-    status_t ret;
-    Parcel data, reply;
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-    EXPECT_EQ(-1, reply.readInt32());
-    EXPECT_EQ(NO_ERROR, ret);
-}
-
-TEST_F(BinderLibTest, WorkSourceSet)
-{
-    status_t ret;
-    Parcel data, reply;
-    IPCThreadState::self()->clearCallingWorkSource();
-    int64_t previousWorkSource = IPCThreadState::self()->setCallingWorkSourceUid(100);
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-    EXPECT_EQ(100, reply.readInt32());
-    EXPECT_EQ(-1, previousWorkSource);
-    EXPECT_EQ(true, IPCThreadState::self()->shouldPropagateWorkSource());
-    EXPECT_EQ(NO_ERROR, ret);
-}
-
-TEST_F(BinderLibTest, WorkSourceSetWithoutPropagation)
-{
-    status_t ret;
-    Parcel data, reply;
-
-    IPCThreadState::self()->setCallingWorkSourceUidWithoutPropagation(100);
-    EXPECT_EQ(false, IPCThreadState::self()->shouldPropagateWorkSource());
-
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-    EXPECT_EQ(-1, reply.readInt32());
-    EXPECT_EQ(false, IPCThreadState::self()->shouldPropagateWorkSource());
-    EXPECT_EQ(NO_ERROR, ret);
-}
-
-TEST_F(BinderLibTest, WorkSourceCleared)
-{
-    status_t ret;
-    Parcel data, reply;
-
-    IPCThreadState::self()->setCallingWorkSourceUid(100);
-    int64_t token = IPCThreadState::self()->clearCallingWorkSource();
-    int32_t previousWorkSource = (int32_t)token;
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-
-    EXPECT_EQ(-1, reply.readInt32());
-    EXPECT_EQ(100, previousWorkSource);
-    EXPECT_EQ(NO_ERROR, ret);
-}
-
-TEST_F(BinderLibTest, WorkSourceRestored)
-{
-    status_t ret;
-    Parcel data, reply;
-
-    IPCThreadState::self()->setCallingWorkSourceUid(100);
-    int64_t token = IPCThreadState::self()->clearCallingWorkSource();
-    IPCThreadState::self()->restoreCallingWorkSource(token);
-
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-
-    EXPECT_EQ(100, reply.readInt32());
-    EXPECT_EQ(true, IPCThreadState::self()->shouldPropagateWorkSource());
-    EXPECT_EQ(NO_ERROR, ret);
-}
-
-TEST_F(BinderLibTest, PropagateFlagSet)
-{
-    IPCThreadState::self()->clearPropagateWorkSource();
-    IPCThreadState::self()->setCallingWorkSourceUid(100);
-    EXPECT_EQ(true, IPCThreadState::self()->shouldPropagateWorkSource());
-}
-
-TEST_F(BinderLibTest, PropagateFlagCleared)
-{
-    IPCThreadState::self()->setCallingWorkSourceUid(100);
-    IPCThreadState::self()->clearPropagateWorkSource();
-    EXPECT_EQ(false, IPCThreadState::self()->shouldPropagateWorkSource());
-}
-
-TEST_F(BinderLibTest, PropagateFlagRestored)
-{
-    int token = IPCThreadState::self()->setCallingWorkSourceUid(100);
-    IPCThreadState::self()->restoreCallingWorkSource(token);
-
-    EXPECT_EQ(false, IPCThreadState::self()->shouldPropagateWorkSource());
-}
-
-TEST_F(BinderLibTest, WorkSourcePropagatedForAllFollowingBinderCalls)
-{
-    IPCThreadState::self()->setCallingWorkSourceUid(100);
-
-    Parcel data, reply;
-    status_t ret;
-    data.writeInterfaceToken(binderLibTestServiceName);
-    ret = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data, &reply);
-
-    Parcel data2, reply2;
-    status_t ret2;
-    data2.writeInterfaceToken(binderLibTestServiceName);
-    ret2 = m_server->transact(BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION, data2, &reply2);
-    EXPECT_EQ(100, reply2.readInt32());
-    EXPECT_EQ(NO_ERROR, ret2);
-}
-
-TEST_F(BinderLibTest, VectorSent) {
-    Parcel data, reply;
-    sp<IBinder> server = addServer();
-    ASSERT_TRUE(server != nullptr);
-
-    std::vector<uint64_t> const testValue = { std::numeric_limits<uint64_t>::max(), 0, 200 };
-    data.writeUint64Vector(testValue);
-
-    status_t ret = server->transact(BINDER_LIB_TEST_ECHO_VECTOR, data, &reply);
-    EXPECT_EQ(NO_ERROR, ret);
-    std::vector<uint64_t> readValue;
-    ret = reply.readUint64Vector(&readValue);
-    EXPECT_EQ(readValue, testValue);
-}
-
 class BinderLibTestService : public BBinder
 {
     public:
@@ -1083,7 +954,6 @@ class BinderLibTestService : public BBinder
             case BINDER_LIB_TEST_ADD_POLL_SERVER:
             case BINDER_LIB_TEST_ADD_SERVER: {
                 int ret;
-                uint8_t buf[1] = { 0 };
                 int serverid;
 
                 if (m_id != 0) {
@@ -1321,19 +1191,6 @@ class BinderLibTestService : public BBinder
                 }
                 return NO_ERROR;
             }
-            case BINDER_LIB_TEST_GET_WORK_SOURCE_TRANSACTION: {
-                data.enforceInterface(binderLibTestServiceName);
-                reply->writeInt32(IPCThreadState::self()->getCallingWorkSourceUid());
-                return NO_ERROR;
-            }
-            case BINDER_LIB_TEST_ECHO_VECTOR: {
-                std::vector<uint64_t> vector;
-                auto err = data.readUint64Vector(&vector);
-                if (err != NO_ERROR)
-                    return err;
-                reply->writeUint64Vector(vector);
-                return NO_ERROR;
-            }
             default:
                 return UNKNOWN_TRANSACTION;
             };
@@ -1346,7 +1203,6 @@ class BinderLibTestService : public BBinder
         bool m_serverStartRequested;
         sp<IBinder> m_serverStarted;
         sp<IBinder> m_strongRef;
-        bool m_callbackPending;
         sp<IBinder> m_callback;
 };
 
@@ -1408,7 +1264,7 @@ int run_server(int index, int readypipefd, bool usePoll)
               * We simulate a single-threaded process using the binder poll
               * interface; besides handling binder commands, it can also
               * issue outgoing transactions, by storing a callback in
-              * m_callback and setting m_callbackPending.
+              * m_callback.
               *
               * processPendingCall() will then issue that transaction.
               */
@@ -1435,8 +1291,6 @@ int run_server(int index, int readypipefd, bool usePoll)
 }
 
 int main(int argc, char **argv) {
-    int ret;
-
     if (argc == 4 && !strcmp(argv[1], "--servername")) {
         binderservername = argv[2];
     } else {

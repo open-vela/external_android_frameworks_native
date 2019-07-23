c990bab2ea521096d3c7d19c4553f4b33e3f2974
liujinye <liujinye@xiaomi.com>
2019-07-23 17:52:04 +0800

    Merge "Improve 'service' command"

--- libs/binder/IAppOpsCallback.cpp
+++ libs/binder/IAppOpsCallback.cpp
@@ -55,8 +55,7 @@ status_t BnAppOpsCallback::onTransact(
         case OP_CHANGED_TRANSACTION: {
             CHECK_INTERFACE(IAppOpsCallback, data, reply);
             int32_t op = data.readInt32();
-            String16 packageName;
-            (void)data.readString16(&packageName);
+            String16 packageName = data.readString16();
             opChanged(op, packageName);
             reply->writeNoException();
             return NO_ERROR;

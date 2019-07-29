764519aa8d984fb9e5d1c8a409014b2460779b7a
openvela-robot <openvela-robot@xiaomi.com>
2019-07-29 21:17:08 +0800

    Merge "libbinder_ndk: default threadpool size is ... 15"

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -218,7 +218,7 @@ void BBinder::setRequestingSid(bool requestingSid)
         if (!e) return; // out of memory
     }
 
-    e->mRequestingSid = requestingSid;
+    e->mRequestingSid = true;
 }
 
 BBinder::~BBinder()

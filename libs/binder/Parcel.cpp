a37d8baadec1214258b340228a7c477f772dcd8b
liujinye <liujinye@xiaomi.com>
2019-08-01 10:32:40 +0800

    Merge "Fix FD comparisons in binder" am: 1a9db5066b
    Change-Id: I76349bd8c44038a0b8efcd88ff0813f2abdca2c4

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -544,7 +544,7 @@ bool Parcel::replaceCallingWorkSourceUid(uid_t uid)
     return err == NO_ERROR;
 }
 
-uid_t Parcel::readCallingWorkSourceUid() const
+uid_t Parcel::readCallingWorkSourceUid()
 {
     if (!mRequestHeaderPresent) {
         return IPCThreadState::kUnsetWorkSource;

494e4558b1e9add4d3c12a7f523d879eaf26b169
openvela-robot <openvela-robot@xiaomi.com>
2019-08-05 18:40:06 +0800

    Merge "libbinder: Fix bad stability set log." am: f6416bb380
    Change-Id: Ibdaa2f7eae2b5dabb062b53a60c802badad6ad82

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -221,7 +221,7 @@ void BBinder::setRequestingSid(bool requestingSid)
         if (!e) return; // out of memory
     }
 
-    e->mRequestingSid = true;
+    e->mRequestingSid = requestingSid;
 }
 
 BBinder::~BBinder()

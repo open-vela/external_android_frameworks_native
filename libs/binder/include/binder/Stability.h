db4ac16dbc25a616f07e1c66459279337344826f
openvela-robot <openvela-robot@xiaomi.com>
2019-08-07 03:15:34 +0800

    Snap for 5784534 from dcaba1b880199996f14e211877f78ff7d7964a42 to rvc-release
    Change-Id: I8e745f7ca33508896feb0d6985df2f808619cc6b

--- libs/binder/include/binder/Stability.h
+++ libs/binder/include/binder/Stability.h
@@ -20,10 +20,6 @@
 #include <string>
 
 namespace android {
-
-class BpBinder;
-class ProcessState;
-
 namespace internal {
 
 // WARNING: These APIs are only ever expected to be called by auto-generated code.
@@ -47,30 +43,14 @@ public:
     // WARNING: for debugging only
     static void debugLogStability(const std::string& tag, const sp<IBinder>& binder);
 
-    // WARNING: This is only ever expected to be called by auto-generated code or tests.
-    // You likely want to change or modify the stability of the interface you are using.
-    // This must be called as soon as the binder in question is constructed. No thread safety
-    // is provided.
-    // E.g. stability is according to libbinder_ndk or Java SDK AND the interface
-    //     expressed here is guaranteed to be stable for multiple years (Stable AIDL)
-    // If this is called when __ANDROID_VNDK__ is not defined, then it is UB and will likely
-    // break the device during GSI or other tests.
-    static void markVndk(IBinder* binder);
-
 private:
-    // Parcel needs to read/write stability level in an unstable format.
+    // Parcel needs to store stability level since this is more efficient than storing and looking
+    // up the efficiency level of a binder object. So, we expose the underlying type.
     friend ::android::Parcel;
-
-    // only expose internal APIs inside of libbinder, for checking stability
-    friend ::android::BpBinder;
-
-    // so that it can mark the context object (only the root object doesn't go
-    // through Parcel)
-    friend ::android::ProcessState;
 
     static void tryMarkCompilationUnit(IBinder* binder);
 
-    enum Level : int32_t {
+    enum Level : int16_t {
         UNDECLARED = 0,
 
         VENDOR = 0b000011,

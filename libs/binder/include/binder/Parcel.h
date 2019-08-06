65b0417bf1477ce46bfc6bb2723d211bc8767437
openvela-robot <openvela-robot@xiaomi.com>
2019-08-07 01:10:48 +0800

    binderStabilityTest: remove unused symbols am: 1db3738568
    Change-Id: Idbaa52cec60bd97c4aa8a2418a8651c22c4487fe

--- libs/binder/include/binder/Parcel.h
+++ libs/binder/include/binder/Parcel.h
@@ -33,7 +33,6 @@
 
 #include <binder/IInterface.h>
 #include <binder/Parcelable.h>
-#include <binder/Stability.h>
 
 // ---------------------------------------------------------------------------
 namespace android {
@@ -68,8 +67,6 @@ public:
     status_t            setDataSize(size_t size);
     void                setDataPosition(size_t pos) const;
     status_t            setDataCapacity(size_t size);
-
-    void                setTransactingBinder(const sp<IBinder>& binder) const;
 
     status_t            setData(const uint8_t* buffer, size_t len);
 
@@ -384,7 +381,7 @@ public:
     bool                replaceCallingWorkSourceUid(uid_t uid);
     // Returns the work source provided by the caller. This can only be trusted for trusted calling
     // uid.
-    uid_t               readCallingWorkSourceUid() const;
+    uid_t               readCallingWorkSourceUid();
     void                readRequestHeaders() const;
 
 private:
@@ -475,15 +472,13 @@ private:
     size_t              mObjectsCapacity;
     mutable size_t      mNextObjectHint;
     mutable bool        mObjectsSorted;
-    bool                mAllowFds;
 
     mutable bool        mRequestHeaderPresent;
     mutable size_t      mWorkSourceRequestHeaderPosition;
 
     mutable bool        mFdsKnown;
     mutable bool        mHasFds;
-
-    mutable internal::Stability::Level mRequiredStability;
+    bool                mAllowFds;
 
     release_func        mOwner;
     void*               mOwnerCookie;

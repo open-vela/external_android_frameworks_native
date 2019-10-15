ad37f8cf3f30bbe97465aac967045f27faad223d
openvela-robot <openvela-robot@xiaomi.com>
2019-10-15 11:49:22 +0800

    Resize object capacity when shrinking Parcel
    Bug: 140419401
    Test: atest android.os.cts.ParcelTest
    Change-Id: I04edee415e1984ba5fb97c5c1b09892a360cf221
    (cherry picked from commit c67d9f33b36cbb95b121d058f51d6653f1ec4334)

--- libs/binder/include/binder/PermissionCache.h
+++ libs/binder/include/binder/PermissionCache.h
@@ -17,8 +17,6 @@
 #ifndef BINDER_PERMISSION_H
 #define BINDER_PERMISSION_H
 
-#ifndef __ANDROID_VNDK__
-
 #include <stdint.h>
 #include <unistd.h>
 
@@ -77,10 +75,6 @@ public:
 };
 
 // ---------------------------------------------------------------------------
-} // namespace android
-
-#else // __ANDROID_VNDK__
-#error "This header is not visible to vendors"
-#endif // __ANDROID_VNDK__
+}; // namespace android
 
 #endif /* BINDER_PERMISSION_H */

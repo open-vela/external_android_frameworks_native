01ab723d77db614d72eaf79b82cc152328480f23
openvela-robot <openvela-robot@xiaomi.com>
2019-12-20 09:03:52 +0800

    Merge "libbinder_ndk: support for nullable parcelables" am: 56ef5878bf
    Change-Id: I6adfa629a8506bab94704f4ab48ecc57f977cf68

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -271,8 +271,6 @@ sp<IBinder> ServiceManagerShim::waitForService(const String16& name16)
             std::unique_lock<std::mutex> lock(mMutex);
             mBinder = binder;
             lock.unlock();
-            // Flushing here helps ensure the service's ref count remains accurate
-            IPCThreadState::self()->flushCommands();
             mCv.notify_one();
             return Status::ok();
         }

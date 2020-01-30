18523fdbe828119b123841031b1c8b0eda29012e
liujinye <liujinye@xiaomi.com>
2020-01-30 22:30:43 +0800

    Merge "libbinder: automatic unlink, print cached desc" am: d35f643711 am: 6d13b79126 am: 7c04a6925c
    Change-Id: I1af58cedef0a5b0bf2cb2a2a53a7cb56e44fff44

--- libs/binder/fuzzer/hwbinder.h
+++ libs/binder/fuzzer/hwbinder.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 The Android Open Source Project
+ * Copyright (C) 2019 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,4 +14,9 @@
  * limitations under the License.
  */
 
-interface IEmpty { }
+#include <hwbinder/Parcel.h>
+#include <vector>
+
+#include "parcel_fuzzer.h"
+
+extern std::vector<ParcelRead<::android::hardware::Parcel>> HWBINDER_PARCEL_READ_FUNCTIONS;

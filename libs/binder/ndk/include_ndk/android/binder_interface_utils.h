17f0619919266cd092e095e93ca37aeb30127e7d
openvela-robot <openvela-robot@xiaomi.com>
2020-02-19 03:21:23 +0800

    Snap for 6216739 from e78aa72ad48bbe6d9ed97c9bfca9f87b64d62787 to rvc-release
    Change-Id: I98444d110cfe9e72124e88e1241cc6d993e9e473

--- libs/binder/ndk/include_ndk/android/binder_interface_utils.h
+++ libs/binder/ndk/include_ndk/android/binder_interface_utils.h
@@ -238,9 +238,7 @@ AIBinder_Class* ICInterface::defineClass(const char* interfaceDescriptor,
     // ourselves. The defaults are harmless.
     AIBinder_Class_setOnDump(clazz, ICInterfaceData::onDump);
 #ifdef HAS_BINDER_SHELL_COMMAND
-    if (AIBinder_Class_setHandleShellCommand != nullptr) {
-        AIBinder_Class_setHandleShellCommand(clazz, ICInterfaceData::handleShellCommand);
-    }
+    AIBinder_Class_setHandleShellCommand(clazz, ICInterfaceData::handleShellCommand);
 #endif
     return clazz;
 }

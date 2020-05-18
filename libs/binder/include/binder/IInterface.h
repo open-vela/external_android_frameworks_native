f6c772879a1725aac6f89acf1f44fb06a2b3a8b6
openvela-robot <openvela-robot@xiaomi.com>
2020-05-18 23:17:34 +0800

    Merge "Revert "Revert "Revert "Make libbinder not available to media.sw..."" into rvc-dev am: 39bf04a268
    Change-Id: Ib52fa066fb2e701c31836e834c16ae5d72f91891

--- libs/binder/include/binder/IInterface.h
+++ libs/binder/include/binder/IInterface.h
@@ -20,8 +20,6 @@
 
 #include <binder/Binder.h>
 
-#include <assert.h>
-
 namespace android {
 
 // ----------------------------------------------------------------------
@@ -157,11 +155,7 @@ public:                                                                 \
     std::unique_ptr<I##INTERFACE> I##INTERFACE::default_impl;           \
     bool I##INTERFACE::setDefaultImpl(std::unique_ptr<I##INTERFACE> impl)\
     {                                                                   \
-        /* Only one user of this interface can use this function     */ \
-        /* at a time. This is a heuristic to detect if two different */ \
-        /* users in the same process use this function.              */ \
-        assert(!I##INTERFACE::default_impl);                            \
-        if (impl) {                                                     \
+        if (!I##INTERFACE::default_impl && impl) {                      \
             I##INTERFACE::default_impl = std::move(impl);               \
             return true;                                                \
         }                                                               \

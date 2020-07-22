1e4fd6e8bb8ee75dc3d39e329ad0abd43877227f
openvela-robot <openvela-robot@xiaomi.com>
2020-07-22 19:02:41 +0800

    [automerger skipped] libbinder: setMinSchedulerPolicy am: 2305b5e9f1 -s ours
    am skip reason: Change-Id Ie93aebb09f4142cd66d841ca8b3ca39d1e0bb75e with SHA-1 01e2da7200 is in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/12197515
    
    Change-Id: If6e9939195408a32bc0b91bce0dda8fde0ec15d5

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -206,6 +206,10 @@ ServiceManagerShim::ServiceManagerShim(const sp<AidlServiceManager>& impl)
  : mTheRealServiceManager(impl)
 {}
 
+// This implementation could be simplified and made more efficient by delegating
+// to waitForService. However, this changes the threading structure in some
+// cases and could potentially break prebuilts. Once we have higher logistical
+// complexity, this could be attempted.
 sp<IBinder> ServiceManagerShim::getService(const String16& name) const
 {
     static bool gSystemBootCompleted = false;
@@ -322,6 +326,11 @@ sp<IBinder> ServiceManagerShim::waitForService(const String16& name16)
 
     while(true) {
         {
+            // It would be really nice if we could read binder commands on this
+            // thread instead of needing a threadpool to be started, but for
+            // instance, if we call getAndExecuteCommand, it might be the case
+            // that another thread serves the callback, and we never get a
+            // command, so we hang indefinitely.
             std::unique_lock<std::mutex> lock(waiter->mMutex);
             using std::literals::chrono_literals::operator""s;
             waiter->mCv.wait_for(lock, 1s, [&] {
@@ -329,6 +338,8 @@ sp<IBinder> ServiceManagerShim::waitForService(const String16& name16)
             });
             if (waiter->mBinder != nullptr) return waiter->mBinder;
         }
+
+        ALOGW("Waited one second for %s (is service started? are binder threads started and available?)", name.c_str());
 
         // Handle race condition for lazy services. Here is what can happen:
         // - the service dies (not processed by init yet).
@@ -343,8 +354,6 @@ sp<IBinder> ServiceManagerShim::waitForService(const String16& name16)
             return nullptr;
         }
         if (out != nullptr) return out;
-
-        ALOGW("Waited one second for %s", name.c_str());
     }
 }
 

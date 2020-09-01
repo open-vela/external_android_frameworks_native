f4851957747d87675a5d05861bfdf4ae848b9362
liujinye <liujinye@xiaomi.com>
2020-09-01 19:52:28 +0800

    Merge "libbinder_ndk: clarify error prune functions" am: 50e004ffa3 am: 347e9e8a8a
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1416528
    
    Change-Id: Ie1e91ccbf7e5ed10b4e4f377cd3e811acbe2cbcd

--- cmds/servicemanager/main.cpp
+++ cmds/servicemanager/main.cpp
@@ -130,7 +130,7 @@ int main(int argc, char** argv) {
     }
 
     IPCThreadState::self()->setTheContextObject(manager);
-    ps->becomeContextManager();
+    ps->becomeContextManager(nullptr, nullptr);
 
     sp<Looper> looper = Looper::prepare(false /*allowNonCallbacks*/);
 

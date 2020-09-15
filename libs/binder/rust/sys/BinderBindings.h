1234c52fe26a02f1107bfa23bb7b1ff0b3fcb22a
openvela-robot <openvela-robot@xiaomi.com>
2020-09-16 00:53:02 +0800

    Merge "Fix Rust string parceling" am: c066ae8e04 am: 86f8606479 am: 776e2f5bef am: c1390d7121 am: f79721ec95
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1382101
    
    Change-Id: I4c418315b5f9fe1339d3efc37ccc0c04c4076be7

--- libs/binder/rust/sys/BinderBindings.h
+++ libs/binder/rust/sys/BinderBindings.h
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#include <android/binder_context.h>
 #include <android/binder_ibinder.h>
 #include <android/binder_manager.h>
 #include <android/binder_parcel.h>

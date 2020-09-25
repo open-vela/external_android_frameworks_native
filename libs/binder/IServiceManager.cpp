fb2cc39e5a6aec7b813e64b96669a924b732536b
liujinye <liujinye@xiaomi.com>
2020-09-25 18:29:58 +0800

    Merge "libbinder_ndk: avoid need to allocate heap status" am: 23790e4178 am: 7a3c7b9bbc
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1434715
    
    Change-Id: I3da056cdfc75a144798a439cefe0631f8a2bdeda

--- libs/binder/IServiceManager.cpp
+++ libs/binder/IServiceManager.cpp
@@ -74,7 +74,6 @@ public:
     Vector<String16> listServices(int dumpsysPriority) override;
     sp<IBinder> waitForService(const String16& name16) override;
     bool isDeclared(const String16& name) override;
-    Vector<String16> getDeclaredInstances(const String16& interface) override;
 
     // for legacy ABI
     const String16& getInterfaceDescriptor() const override {
@@ -372,20 +371,6 @@ bool ServiceManagerShim::isDeclared(const String16& name) {
         return false;
     }
     return declared;
-}
-
-Vector<String16> ServiceManagerShim::getDeclaredInstances(const String16& interface) {
-    std::vector<std::string> out;
-    if (!mTheRealServiceManager->getDeclaredInstances(String8(interface).c_str(), &out).isOk()) {
-        return {};
-    }
-
-    Vector<String16> res;
-    res.setCapacity(out.size());
-    for (const std::string& instance : out) {
-        res.push(String16(instance.c_str()));
-    }
-    return res;
 }
 
 } // namespace android

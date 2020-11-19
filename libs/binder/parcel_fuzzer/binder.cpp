7afd064beae9c13955f42403e4e13fa7720a3ccb
liujinye <liujinye@xiaomi.com>
2020-11-20 01:10:53 +0800

    Merge "Move Rust Binder tests into libbinder TEST_MAPPING" am: 367f449eea
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1506416
    
    Change-Id: Ibabe50b26bcd41e77bde2956945591c000b29d82

--- libs/binder/parcel_fuzzer/binder.cpp
+++ libs/binder/parcel_fuzzer/binder.cpp
@@ -86,8 +86,6 @@ struct ExampleLightFlattenable : public android::LightFlattenablePod<ExampleLigh
     PARCEL_READ_WITH_STATUS(T, FUN), \
     PARCEL_READ_NO_STATUS(T, FUN)
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 // clang-format off
 std::vector<ParcelRead<::android::Parcel>> BINDER_PARCEL_READ_FUNCTIONS {
     PARCEL_READ_NO_STATUS(size_t, dataSize),
@@ -291,4 +289,3 @@ std::vector<ParcelRead<::android::Parcel>> BINDER_PARCEL_READ_FUNCTIONS {
     PARCEL_READ_WITH_STATUS(android::os::PersistableBundle, readParcelable),
 };
 // clang-format on
-#pragma clang diagnostic pop

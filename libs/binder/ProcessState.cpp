b5f78cf079a65b2bac96568cfb2d64e9eb93acbe
openvela-robot <openvela-robot@xiaomi.com>
2020-12-02 03:09:54 +0800

    Merge "libbinder_rs: Treat previously associated Binder as remote" am: ba53253ded am: d55cf27d46 am: c4c902bf4e
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1424936
    
    Change-Id: Id04c64d23638808bbf0d2de41e7774ed024e5b36

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -130,7 +130,7 @@ sp<IBinder> ProcessState::getContextObject(const sp<IBinder>& /*caller*/)
 
     // The root object is special since we get it directly from the driver, it is never
     // written by Parcell::writeStrongBinder.
-    internal::Stability::markCompilationUnit(context.get());
+    internal::Stability::tryMarkCompilationUnit(context.get());
 
     return context;
 }

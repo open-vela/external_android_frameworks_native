0e7cfc340020ed645dcd951cb67c3d3adeffc5b6
openvela-robot <openvela-robot@xiaomi.com>
2020-12-30 19:37:25 +0800

    Merge "libbinder: only set sm stability if non-null" am: 7ac860e54f
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1536623
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ibde722a1586ee853ab4209ca2c3643bd63051e42

--- libs/binder/ndk/include_cpp/android/binder_parcelable_utils.h
+++ libs/binder/ndk/include_cpp/android/binder_parcelable_utils.h
@@ -114,12 +114,24 @@ class AParcelableHolder {
 
     void reset() { AParcel_reset(mParcel.get()); }
 
-    inline bool operator!=(const AParcelableHolder& rhs) const { return this != &rhs; }
-    inline bool operator<(const AParcelableHolder& rhs) const { return this < &rhs; }
-    inline bool operator<=(const AParcelableHolder& rhs) const { return this <= &rhs; }
-    inline bool operator==(const AParcelableHolder& rhs) const { return this == &rhs; }
-    inline bool operator>(const AParcelableHolder& rhs) const { return this > &rhs; }
-    inline bool operator>=(const AParcelableHolder& rhs) const { return this >= &rhs; }
+    inline bool operator!=(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) != std::tie(rhs.mParcel, rhs.mStability);
+    }
+    inline bool operator<(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) < std::tie(rhs.mParcel, rhs.mStability);
+    }
+    inline bool operator<=(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) <= std::tie(rhs.mParcel, rhs.mStability);
+    }
+    inline bool operator==(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) == std::tie(rhs.mParcel, rhs.mStability);
+    }
+    inline bool operator>(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) > std::tie(rhs.mParcel, rhs.mStability);
+    }
+    inline bool operator>=(const AParcelableHolder& rhs) const {
+        return std::tie(mParcel, mStability) >= std::tie(rhs.mParcel, rhs.mStability);
+    }
 
    private:
     mutable ndk::ScopedAParcel mParcel;

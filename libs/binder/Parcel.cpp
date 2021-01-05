c7430680e637a2dd87e5dfc1c6ebf960b4e3514c
openvela-robot <openvela-robot@xiaomi.com>
2021-01-05 10:29:57 +0800

    Merge RQ1A.210105.003 to stage-aosp-master - DO NOT MERGE
    Merged-In: I3c36d6f26c6a855a890329fc8c5229e5946b2d36
    Change-Id: Id97e4f718debccece7fe73d87feb00c892ac1c6b

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -2059,10 +2059,7 @@ const char* Parcel::readString8Inplace(size_t* outLen) const
         *outLen = size;
         const char* str = (const char*)readInplace(size+1);
         if (str != nullptr) {
-            if (str[size] == '\0') {
-                return str;
-            }
-            android_errorWriteLog(0x534e4554, "172655291");
+            return str;
         }
     }
     *outLen = 0;
@@ -2145,10 +2142,7 @@ const char16_t* Parcel::readString16Inplace(size_t* outLen) const
         *outLen = size;
         const char16_t* str = (const char16_t*)readInplace((size+1)*sizeof(char16_t));
         if (str != nullptr) {
-            if (str[size] == u'\0') {
-                return str;
-            }
-            android_errorWriteLog(0x534e4554, "172655291");
+            return str;
         }
     }
     *outLen = 0;

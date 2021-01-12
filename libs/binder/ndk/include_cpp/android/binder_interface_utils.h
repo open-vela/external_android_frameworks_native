c2811949df128e096a9b4f5e33bb8dcb9c267939
openvela-robot <openvela-robot@xiaomi.com>
2021-01-12 17:45:09 +0800

    Merge "Check FLAG_CLEAR_BUF behavior."

--- libs/binder/ndk/include_cpp/android/binder_interface_utils.h
+++ libs/binder/ndk/include_cpp/android/binder_interface_utils.h
@@ -246,7 +246,7 @@ AIBinder_Class* ICInterface::defineClass(const char* interfaceDescriptor,
     // ourselves. The defaults are harmless.
     AIBinder_Class_setOnDump(clazz, ICInterfaceData::onDump);
 #ifdef HAS_BINDER_SHELL_COMMAND
-    if (__builtin_available(android 30, *)) {
+    if (AIBinder_Class_setHandleShellCommand != nullptr) {
         AIBinder_Class_setHandleShellCommand(clazz, ICInterfaceData::handleShellCommand);
     }
 #endif

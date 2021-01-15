f62475408d8d1875f565550e768381591a75dcd6
openvela-robot <openvela-robot@xiaomi.com>
2021-01-15 23:50:39 +0800

    [automerger skipped] Merge "Add active services count callback to lazy services." am: 1de2582455 -s ours am: 30acc4a133 -s ours
    am skip reason: Change-Id I9aa3c9b681bd340ca340fe7ed818ba3533678af2 with SHA-1 96426e49ce is in history
    
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1512817
    
    MUST ONLY BE SUBMITTED BY AUTOMERGER
    
    Change-Id: Ibb94c215ec13f706a309061388d8dd3db30e2265

--- libs/binder/include/binder/IPCThreadState.h
+++ libs/binder/include/binder/IPCThreadState.h
@@ -110,7 +110,6 @@ public:
             status_t            setupPolling(int* fd);
             status_t            handlePolledCommands();
             void                flushCommands();
-            bool                flushIfNeeded();
 
             void                joinThreadPool(bool isMain = true);
             
@@ -205,7 +204,6 @@ private:
             int32_t             mWorkSource;
             // Whether the work source should be propagated.
             bool                mPropagateWorkSource;
-            bool                mIsLooper;
             int32_t             mStrictModePolicy;
             int32_t             mLastTransactionBinderFlags;
             CallRestriction     mCallRestriction;

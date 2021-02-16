126b775edf97d59b449c41da792ecfc0d79bbcf9
liujinye <liujinye@xiaomi.com>
2021-02-16 14:50:56 +0800

    Merge "[LSC] Add LOCAL_LICENSE_KINDS to frameworks/native"

--- libs/binder/rust/tests/serialization.rs
+++ libs/binder/rust/tests/serialization.rs
@@ -40,41 +40,6 @@ mod bindings {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 }
 
-macro_rules! assert_eq {
-    ($left:expr, $right:expr $(,)?) => {
-        match (&$left, &$right) {
-            (left, right) => {
-                if *left != *right {
-                    eprintln!(
-                        "assertion failed: `{:?}` == `{:?}`, {}:{}:{}",
-                        &*left,
-                        &*right,
-                        file!(),
-                        line!(),
-                        column!()
-                    );
-                    return Err(StatusCode::FAILED_TRANSACTION);
-                }
-            }
-        }
-    };
-}
-
-macro_rules! assert {
-    ($expr:expr) => {
-        if !$expr {
-            eprintln!(
-                "assertion failed: `{:?}`, {}:{}:{}",
-                $expr,
-                file!(),
-                line!(),
-                column!()
-            );
-            return Err(StatusCode::FAILED_TRANSACTION);
-        }
-    };
-}
-
 static SERVICE_ONCE: Once = Once::new();
 static mut SERVICE: Option<SpIBinder> = None;
 
@@ -317,7 +282,7 @@ fn on_transact(_service: &dyn ReadParcelTest, code: TransactionCode,
             ))?;
         }
         bindings::Transaction_TEST_FAIL => {
-            assert!(false);
+            return Err(StatusCode::FAILED_TRANSACTION)
         }
         _ => return Err(StatusCode::UNKNOWN_TRANSACTION),
     }

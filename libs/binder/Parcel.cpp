6cc56fddc36b430409418e3188317d7fae733c29
liujinye <liujinye@xiaomi.com>
2021-02-16 23:27:24 +0800

    Merge "Make libbinder_rs be available to the virt APEX"

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -1651,10 +1651,7 @@ const char* Parcel::readString8Inplace(size_t* outLen) const
         *outLen = size;
         const char* str = (const char*)readInplace(size+1);
         if (str != nullptr) {
-            if (str[size] == '\0') {
-                return str;
-            }
-            android_errorWriteLog(0x534e4554, "172655291");
+            return str;
         }
     }
     *outLen = 0;
@@ -1692,10 +1689,7 @@ const char16_t* Parcel::readString16Inplace(size_t* outLen) const
         *outLen = size;
         const char16_t* str = (const char16_t*)readInplace((size+1)*sizeof(char16_t));
         if (str != nullptr) {
-            if (str[size] == u'\0') {
-                return str;
-            }
-            android_errorWriteLog(0x534e4554, "172655291");
+            return str;
         }
     }
     *outLen = 0;

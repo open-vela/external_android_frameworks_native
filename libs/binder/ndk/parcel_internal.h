a0161addeb84a83b593651118e322a4f5b615b7f
openvela-robot <openvela-robot@xiaomi.com>
2021-04-02 14:46:01 +0800

    Merge "Add a new uwb ranging app-ops" into sc-dev

--- libs/binder/ndk/parcel_internal.h
+++ libs/binder/ndk/parcel_internal.h
@@ -27,13 +27,10 @@ struct AParcel {
     const ::android::Parcel* get() const { return mParcel; }
     ::android::Parcel* get() { return mParcel; }
 
-    explicit AParcel(AIBinder* binder) : AParcel(binder, new ::android::Parcel, true /*owns*/) {}
-    AParcel(AIBinder* binder, ::android::Parcel* parcel, bool owns)
-        : mBinder(binder), mParcel(parcel), mOwns(owns) {
-        if (binder != nullptr) {
-            parcel->markForBinder(binder->getBinder());
-        }
-    }
+    explicit AParcel(const AIBinder* binder)
+        : AParcel(binder, new ::android::Parcel, true /*owns*/) {}
+    AParcel(const AIBinder* binder, ::android::Parcel* parcel, bool owns)
+        : mBinder(binder), mParcel(parcel), mOwns(owns) {}
 
     ~AParcel() {
         if (mOwns) {
@@ -41,7 +38,7 @@ struct AParcel {
         }
     }
 
-    static const AParcel readOnly(AIBinder* binder, const ::android::Parcel* parcel) {
+    static const AParcel readOnly(const AIBinder* binder, const ::android::Parcel* parcel) {
         return AParcel(binder, const_cast<::android::Parcel*>(parcel), false);
     }
 

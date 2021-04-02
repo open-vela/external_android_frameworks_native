a59d4064c9f6172f6d1b51eb4f68aed32734fe4c
openvela-robot <openvela-robot@xiaomi.com>
2021-04-03 00:35:06 +0800

    Merge "libbinder: split out ProcessInfoService" am: 18e6279727 am: 294871ba1f
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1663089
    
    Change-Id: Ibc6026e9376479b3a13326cbeccc80d1cd81fc1f

--- libs/binder/tests/binderStabilityTest.cpp
+++ libs/binder/tests/binderStabilityTest.cpp
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 
-#include <android/binder_libbinder.h>
 #include <android/binder_manager.h>
 #include <android/binder_stability.h>
 #include <binder/Binder.h>
@@ -138,18 +137,7 @@ TEST(BinderStability, ForceDowngradeStability) {
     EXPECT_TRUE(Stability::requiresVintfDeclaration(someBinder));
 
     // silly to do this after already using the binder, but it's for the test
-    Stability::forceDowngradeToLocalStability(someBinder);
-
-    EXPECT_FALSE(Stability::requiresVintfDeclaration(someBinder));
-}
-
-TEST(BinderStability, NdkForceDowngradeStability) {
-    sp<IBinder> someBinder = BadStableBinder::vintf();
-
-    EXPECT_TRUE(Stability::requiresVintfDeclaration(someBinder));
-
-    // silly to do this after already using the binder, but it's for the test
-    AIBinder_forceDowngradeToLocalStability(AIBinder_fromPlatformBinder(someBinder));
+    Stability::forceDowngradeCompilationUnit(someBinder);
 
     EXPECT_FALSE(Stability::requiresVintfDeclaration(someBinder));
 }

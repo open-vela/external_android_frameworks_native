d485f5cbe98c778ebcfb780f9a7f7a9f3a55c972
openvela-robot <openvela-robot@xiaomi.com>
2021-05-03 21:51:15 +0800

    Merge changes If8563c69,I48821970,Ic79479b6,I74b04185 am: 52b483f5eb am: cc8c209519
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1692849
    
    Change-Id: I74bc54b9f40d60353b6bec7a95e5ebf3d00320ac

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -78,11 +78,7 @@ static size_t pad_size(size_t s) {
 namespace android {
 
 // many things compile this into prebuilts on the stack
-#ifdef __LP64__
-static_assert(sizeof(Parcel) == 120);
-#else
-static_assert(sizeof(Parcel) == 60);
-#endif
+static_assert(sizeof(Parcel) == 60 || sizeof(Parcel) == 120);
 
 static std::atomic<size_t> gParcelGlobalAllocCount;
 static std::atomic<size_t> gParcelGlobalAllocSize;

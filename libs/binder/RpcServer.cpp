1ef9f784e46df86a00534e5a557fd1b7424ca2da
liujinye <liujinye@xiaomi.com>
2021-05-10 16:28:54 +0800

    Make available to com.android.compos apex
    Bug: 186126404
    Test: m
    
    Change-Id: Ie649a17a3fae4f2bb668d8a084b9945716e2a48e
    (cherry picked from commit fe07356e6b98e5aacda68a765fea671c3a65790e)

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -16,21 +16,19 @@
 
 #define LOG_TAG "RpcServer"
 
-#include <poll.h>
 #include <sys/socket.h>
 #include <sys/un.h>
 
 #include <thread>
 #include <vector>
 
-#include <android-base/macros.h>
 #include <android-base/scopeguard.h>
 #include <binder/Parcel.h>
 #include <binder/RpcServer.h>
 #include <log/log.h>
+#include "RpcState.h"
 
 #include "RpcSocketAddress.h"
-#include "RpcState.h"
 #include "RpcWireFormat.h"
 
 namespace android {
@@ -101,7 +99,7 @@ bool RpcServer::setupInetServer(unsigned int port, unsigned int* assignedPort) {
 
 void RpcServer::setMaxThreads(size_t threads) {
     LOG_ALWAYS_FATAL_IF(threads <= 0, "RpcServer is useless without threads");
-    LOG_ALWAYS_FATAL_IF(mJoinThreadRunning, "Cannot set max threads while running");
+    LOG_ALWAYS_FATAL_IF(mStarted, "must be called before started");
     mMaxThreads = threads;
 }
 
@@ -128,61 +126,16 @@ sp<IBinder> RpcServer::getRootObject() {
     return ret;
 }
 
-std::unique_ptr<RpcServer::FdTrigger> RpcServer::FdTrigger::make() {
-    auto ret = std::make_unique<RpcServer::FdTrigger>();
-    if (!android::base::Pipe(&ret->mRead, &ret->mWrite)) return nullptr;
-    return ret;
-}
-
-void RpcServer::FdTrigger::trigger() {
-    mWrite.reset();
-}
-
 void RpcServer::join() {
-    LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
-
-    {
-        std::lock_guard<std::mutex> _l(mLock);
-        LOG_ALWAYS_FATAL_IF(!mServer.ok(), "RpcServer must be setup to join.");
-        LOG_ALWAYS_FATAL_IF(mShutdownTrigger != nullptr, "Already joined");
-        mJoinThreadRunning = true;
-        mShutdownTrigger = FdTrigger::make();
-        LOG_ALWAYS_FATAL_IF(mShutdownTrigger == nullptr, "Cannot create join signaler");
-    }
-
     while (true) {
-        pollfd pfd[]{{.fd = mServer.get(), .events = POLLIN, .revents = 0},
-                     {.fd = mShutdownTrigger->readFd().get(), .events = POLLHUP, .revents = 0}};
-        int ret = TEMP_FAILURE_RETRY(poll(pfd, arraysize(pfd), -1));
-        if (ret < 0) {
-            ALOGE("Could not poll socket: %s", strerror(errno));
-            continue;
-        }
-        if (ret == 0) {
-            continue;
-        }
-        if (pfd[1].revents & POLLHUP) {
-            LOG_RPC_DETAIL("join() exiting because shutdown requested.");
-            break;
-        }
-
-        (void)acceptOneNoCheck();
+        (void)acceptOne();
     }
-
-    {
-        std::lock_guard<std::mutex> _l(mLock);
-        mJoinThreadRunning = false;
-    }
-    mShutdownCv.notify_all();
 }
 
 bool RpcServer::acceptOne() {
     LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
-    LOG_ALWAYS_FATAL_IF(!hasServer(), "RpcServer must be setup to acceptOne.");
-    return acceptOneNoCheck();
-}
+    LOG_ALWAYS_FATAL_IF(!hasServer(), "RpcServer must be setup to join.");
 
-bool RpcServer::acceptOneNoCheck() {
     unique_fd clientFd(
             TEMP_FAILURE_RETRY(accept4(mServer.get(), nullptr, nullptr /*length*/, SOCK_CLOEXEC)));
 
@@ -200,18 +153,6 @@ bool RpcServer::acceptOneNoCheck() {
         mConnectingThreads[thread.get_id()] = std::move(thread);
     }
 
-    return true;
-}
-
-bool RpcServer::shutdown() {
-    LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
-    std::unique_lock<std::mutex> _l(mLock);
-    if (mShutdownTrigger == nullptr) return false;
-
-    mShutdownTrigger->trigger();
-    while (mJoinThreadRunning) mShutdownCv.wait(_l);
-
-    mShutdownTrigger = nullptr;
     return true;
 }
 
@@ -314,10 +255,7 @@ bool RpcServer::setupSocketServer(const RpcSocketAddress& addr) {
 
     LOG_RPC_DETAIL("Successfully setup socket server %s", addr.toString().c_str());
 
-    if (!setupExternalServer(std::move(serverFd))) {
-        ALOGE("Another thread has set up server while calling setupSocketServer. Race?");
-        return false;
-    }
+    mServer = std::move(serverFd);
     return true;
 }
 

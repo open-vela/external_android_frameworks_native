d0ae10fffec0b4e9cd71c04ad1b69f803ef705d5
openvela-robot <openvela-robot@xiaomi.com>
2021-05-11 16:38:42 +0800

    Merge "libbinder: organize fuzzers" into sc-dev

--- libs/binder/include/binder/RpcServer.h
+++ libs/binder/include/binder/RpcServer.h
@@ -57,10 +57,12 @@ public:
      */
     [[nodiscard]] bool setupUnixDomainServer(const char* path);
 
+#ifdef __BIONIC__
     /**
      * Creates an RPC server at the current port.
      */
     [[nodiscard]] bool setupVsockServer(unsigned int port);
+#endif // __BIONIC__
 
     /**
      * Creates an RPC server at the current port using IPv4.

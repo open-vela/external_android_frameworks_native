91ef5a8b04ab474fafcb0774a66547a4f0cd9377
openvela-robot <openvela-robot@xiaomi.com>
2021-05-11 16:50:29 +0800

    Merge changes I9b5b2876,I3b93e0a9 am: 70557f7990
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1697653
    
    Change-Id: I05b63b429cd2da1f2ed5856a4d2d1593feac7484

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -49,16 +49,12 @@ bool RpcServer::setupUnixDomainServer(const char* path) {
     return setupSocketServer(UnixSocketAddress(path));
 }
 
-#ifdef __BIONIC__
-
 bool RpcServer::setupVsockServer(unsigned int port) {
     // realizing value w/ this type at compile time to avoid ubsan abort
     constexpr unsigned int kAnyCid = VMADDR_CID_ANY;
 
     return setupSocketServer(VsockSocketAddress(kAnyCid, port));
 }
-
-#endif // __BIONIC__
 
 bool RpcServer::setupInetServer(unsigned int port, unsigned int* assignedPort) {
     const char* kAddr = "127.0.0.1";
@@ -129,8 +125,8 @@ void RpcServer::join() {
     }
 
     while (true) {
-        unique_fd clientFd(
-                TEMP_FAILURE_RETRY(accept4(mServer.get(), nullptr, 0 /*length*/, SOCK_CLOEXEC)));
+        unique_fd clientFd(TEMP_FAILURE_RETRY(
+                accept4(mServer.get(), nullptr, nullptr /*length*/, SOCK_CLOEXEC)));
 
         if (clientFd < 0) {
             ALOGE("Could not accept4 socket: %s", strerror(errno));

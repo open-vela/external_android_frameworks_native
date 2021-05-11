de82b5865326a04bba53f79dd3c65d3f78d68f89
openvela-robot <openvela-robot@xiaomi.com>
2021-05-11 17:11:24 +0800

    Merge changes I9b5b2876,I3b93e0a9 am: 70557f7990 am: b5d7b08c19
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1697653
    
    Change-Id: Ie4e84581e417b39b6a0b81b4e1d0864e731bce46

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -134,8 +134,7 @@ void RpcServer::join() {
         }
         LOG_RPC_DETAIL("accept4 on fd %d yields fd %d", mServer.get(), clientFd.get());
 
-        // TODO(b/183988761): cannot trust this simple ID, should not block this
-        // thread
+        // TODO(b/183988761): cannot trust this simple ID
         LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
         int32_t id;
         if (sizeof(id) != read(clientFd.get(), &id, sizeof(id))) {

a25c2200f0213bfc4df166dd107e167b513d288a
liujinye <liujinye@xiaomi.com>
2021-05-14 03:22:53 +0800

    Merge "libbinder: more forcePersist docs"

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -255,10 +255,7 @@ bool RpcServer::setupSocketServer(const RpcSocketAddress& addr) {
 
     LOG_RPC_DETAIL("Successfully setup socket server %s", addr.toString().c_str());
 
-    if (!setupExternalServer(std::move(serverFd))) {
-        ALOGE("Another thread has set up server while calling setupSocketServer. Race?");
-        return false;
-    }
+    mServer = std::move(serverFd);
     return true;
 }
 
@@ -275,26 +272,8 @@ void RpcServer::onSessionTerminating(const sp<RpcSession>& session) {
 }
 
 bool RpcServer::hasServer() {
-    LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
     std::lock_guard<std::mutex> _l(mLock);
     return mServer.ok();
-}
-
-unique_fd RpcServer::releaseServer() {
-    LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
-    std::lock_guard<std::mutex> _l(mLock);
-    return std::move(mServer);
-}
-
-bool RpcServer::setupExternalServer(base::unique_fd serverFd) {
-    LOG_ALWAYS_FATAL_IF(!mAgreedExperimental, "no!");
-    std::lock_guard<std::mutex> _l(mLock);
-    if (mServer.ok()) {
-        ALOGE("Each RpcServer can only have one server.");
-        return false;
-    }
-    mServer = std::move(serverFd);
-    return true;
 }
 
 } // namespace android

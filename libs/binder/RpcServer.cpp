fab2fbec6a06cb12bd5593418bf1b373a52a1c26
openvela-robot <openvela-robot@xiaomi.com>
2021-05-14 23:02:07 +0800

    Revert "libbinder: binder RPC - using getCalling* aborts"
    Revert submission 1707789
    
    Reason for revert: potential cause of b/188228705
    Reverted Changes:
    Id8fc889f4:libbinder: binder RPC - using getCalling* aborts
    I2145ad0e7:libbinder: introduce guards for getCalling*
    
    Bug: 188228705
    
    Change-Id: I4cba7ab9a2a525c6f8158e32c8411ce081e0a8a2
    Merged-In: I4cba7ab9a2a525c6f8158e32c8411ce081e0a8a2

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -255,10 +255,7 @@ bool RpcServer::setupSocketServer(const RpcSocketAddress& addr) {
 
     LOG_RPC_DETAIL("Successfully setup socket server %s", addr.toString().c_str());
 
-    if (!setupExternalServer(std::move(serverFd))) {
-        ALOGE("Another thread has set up server while calling setupSocketServer. Race?");
-        return false;
-    }
+    mServer = std::move(serverFd);
     return true;
 }
 

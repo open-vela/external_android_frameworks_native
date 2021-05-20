111905f314efab0cfa804e5c1ef845dc00cb9bf2
openvela-robot <openvela-robot@xiaomi.com>
2021-05-20 22:01:46 +0800

    Merge changes Ide3d1dd5,I0d1cc5ba am: 00dfc4220d
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1712595
    
    Change-Id: I85ff13331691b3fe4c5c96ba61f8e123c114f9ff

--- libs/binder/include/binder/IPCThreadState.h
+++ libs/binder/include/binder/IPCThreadState.h
@@ -243,7 +243,6 @@ private:
             // Whether the work source should be propagated.
             bool                mPropagateWorkSource;
             bool                mIsLooper;
-            bool mIsFlushing;
             int32_t             mStrictModePolicy;
             int32_t             mLastTransactionBinderFlags;
             CallRestriction     mCallRestriction;

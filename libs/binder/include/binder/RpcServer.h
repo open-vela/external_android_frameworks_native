7c93b46770cd4bbd9bc02325b02d6d57bd3b6717
Steven Moreland <smoreland@google.com>
2021-05-20 01:18:54 +0000

    libbinder: RpcServer privatize acceptOne
    This doesn't configure RpcServer w/ a way to be shutdown, and we don't
    really need it, so simplifying things.
    
    Bug: 185167543
    Test: N/A
    Change-Id: I7648ff04f6096f215a90caf3b9f21060eca882ac

--- libs/binder/include/binder/RpcServer.h
+++ libs/binder/include/binder/RpcServer.h
@@ -135,12 +135,6 @@ public:
     [[nodiscard]] bool shutdown();
 
     /**
-     * Accept one connection on this server. You must have at least one client
-     * session before calling this.
-     */
-    [[nodiscard]] bool acceptOne();
-
-    /**
      * For debugging!
      */
     std::vector<sp<RpcSession>> listSessions();
@@ -158,7 +152,7 @@ private:
 
     static void establishConnection(sp<RpcServer>&& server, base::unique_fd clientFd);
     bool setupSocketServer(const RpcSocketAddress& address);
-    [[nodiscard]] bool acceptOneNoCheck();
+    [[nodiscard]] bool acceptOne();
 
     bool mAgreedExperimental = false;
     size_t mMaxThreads = 1;

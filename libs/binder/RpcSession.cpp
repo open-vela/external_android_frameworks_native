e96577a57b4a453d4d3adea2925d0fa377ffadd5
openvela-robot <openvela-robot@xiaomi.com>
2021-05-26 00:38:09 +0800

    Merge "libbinder: further downgrade oneway spam log" into sc-dev am: 00d402c140
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/14717020
    
    Change-Id: I03abfc6cfff1e0a1d70b1f130fd7e4fa7d46e0d6

--- libs/binder/RpcSession.cpp
+++ libs/binder/RpcSession.cpp
@@ -19,12 +19,10 @@
 #include <binder/RpcSession.h>
 
 #include <inttypes.h>
-#include <poll.h>
 #include <unistd.h>
 
 #include <string_view>
 
-#include <android-base/macros.h>
 #include <binder/Parcel.h>
 #include <binder/RpcServer.h>
 #include <binder/Stability.h>
@@ -86,7 +84,8 @@ bool RpcSession::addNullDebuggingClient() {
         return false;
     }
 
-    return addClientConnection(std::move(serverFd));
+    addClientConnection(std::move(serverFd));
+    return true;
 }
 
 sp<IBinder> RpcSession::getRootObject() {
@@ -99,12 +98,12 @@ status_t RpcSession::getRemoteMaxThreads(size_t* maxThreads) {
     return state()->getMaxThreads(connection.fd(), sp<RpcSession>::fromExisting(this), maxThreads);
 }
 
-status_t RpcSession::transact(const sp<IBinder>& binder, uint32_t code, const Parcel& data,
+status_t RpcSession::transact(const RpcAddress& address, uint32_t code, const Parcel& data,
                               Parcel* reply, uint32_t flags) {
     ExclusiveConnection connection(sp<RpcSession>::fromExisting(this),
                                    (flags & IBinder::FLAG_ONEWAY) ? ConnectionUse::CLIENT_ASYNC
                                                                   : ConnectionUse::CLIENT);
-    return state()->transact(connection.fd(), binder, code, data,
+    return state()->transact(connection.fd(), address, code, data,
                              sp<RpcSession>::fromExisting(this), reply, flags);
 }
 
@@ -112,53 +111,6 @@ status_t RpcSession::sendDecStrong(const RpcAddress& address) {
     ExclusiveConnection connection(sp<RpcSession>::fromExisting(this),
                                    ConnectionUse::CLIENT_REFCOUNT);
     return state()->sendDecStrong(connection.fd(), address);
-}
-
-std::unique_ptr<RpcSession::FdTrigger> RpcSession::FdTrigger::make() {
-    auto ret = std::make_unique<RpcSession::FdTrigger>();
-    if (!android::base::Pipe(&ret->mRead, &ret->mWrite)) return nullptr;
-    return ret;
-}
-
-void RpcSession::FdTrigger::trigger() {
-    mWrite.reset();
-}
-
-status_t RpcSession::FdTrigger::triggerablePollRead(base::borrowed_fd fd) {
-    while (true) {
-        pollfd pfd[]{{.fd = fd.get(), .events = POLLIN | POLLHUP, .revents = 0},
-                     {.fd = mRead.get(), .events = POLLHUP, .revents = 0}};
-        int ret = TEMP_FAILURE_RETRY(poll(pfd, arraysize(pfd), -1));
-        if (ret < 0) {
-            return -errno;
-        }
-        if (ret == 0) {
-            continue;
-        }
-        if (pfd[1].revents & POLLHUP) {
-            return -ECANCELED;
-        }
-        return pfd[0].revents & POLLIN ? OK : DEAD_OBJECT;
-    }
-}
-
-status_t RpcSession::FdTrigger::interruptableReadFully(base::borrowed_fd fd, void* data,
-                                                       size_t size) {
-    uint8_t* buffer = reinterpret_cast<uint8_t*>(data);
-    uint8_t* end = buffer + size;
-
-    status_t status;
-    while ((status = triggerablePollRead(fd)) == OK) {
-        ssize_t readSize = TEMP_FAILURE_RETRY(recv(fd.get(), buffer, end - buffer, MSG_NOSIGNAL));
-        if (readSize == 0) return DEAD_OBJECT; // EOF
-
-        if (readSize < 0) {
-            return -errno;
-        }
-        buffer += readSize;
-        if (buffer == end) return OK;
-    }
-    return status;
 }
 
 status_t RpcSession::readId() {
@@ -198,8 +150,7 @@ void RpcSession::join(unique_fd client) {
                 state()->getAndExecuteCommand(connection->fd, sp<RpcSession>::fromExisting(this));
 
         if (error != OK) {
-            LOG_RPC_DETAIL("Binder connection thread closing w/ status %s",
-                           statusToString(error).c_str());
+            ALOGI("Binder connection thread closing w/ status %s", statusToString(error).c_str());
             break;
         }
     }
@@ -207,19 +158,12 @@ void RpcSession::join(unique_fd client) {
     LOG_ALWAYS_FATAL_IF(!removeServerConnection(connection),
                         "bad state: connection object guaranteed to be in list");
 
-    sp<RpcServer> server;
     {
         std::lock_guard<std::mutex> _l(mMutex);
         auto it = mThreads.find(std::this_thread::get_id());
         LOG_ALWAYS_FATAL_IF(it == mThreads.end());
         it->second.detach();
         mThreads.erase(it);
-
-        server = mForServer.promote();
-    }
-
-    if (server != nullptr) {
-        server->onSessionThreadEnding(sp<RpcSession>::fromExisting(this));
     }
 }
 
@@ -311,36 +255,24 @@ bool RpcSession::setupOneSocketClient(const RpcSocketAddress& addr, int32_t id) 
 
         LOG_RPC_DETAIL("Socket at %s client with fd %d", addr.toString().c_str(), serverFd.get());
 
-        return addClientConnection(std::move(serverFd));
+        addClientConnection(std::move(serverFd));
+        return true;
     }
 
     ALOGE("Ran out of retries to connect to %s", addr.toString().c_str());
     return false;
 }
 
-bool RpcSession::addClientConnection(unique_fd fd) {
+void RpcSession::addClientConnection(unique_fd fd) {
     std::lock_guard<std::mutex> _l(mMutex);
-
-    if (mShutdownTrigger == nullptr) {
-        mShutdownTrigger = FdTrigger::make();
-        if (mShutdownTrigger == nullptr) return false;
-    }
-
     sp<RpcConnection> session = sp<RpcConnection>::make();
     session->fd = std::move(fd);
     mClientConnections.push_back(session);
-    return true;
 }
 
-void RpcSession::setForServer(const wp<RpcServer>& server, int32_t sessionId,
-                              const std::shared_ptr<FdTrigger>& shutdownTrigger) {
-    LOG_ALWAYS_FATAL_IF(mForServer.unsafe_get() != nullptr);
-    LOG_ALWAYS_FATAL_IF(mShutdownTrigger != nullptr);
-    LOG_ALWAYS_FATAL_IF(shutdownTrigger == nullptr);
-
+void RpcSession::setForServer(const wp<RpcServer>& server, int32_t sessionId) {
     mId = sessionId;
     mForServer = server;
-    mShutdownTrigger = shutdownTrigger;
 }
 
 sp<RpcSession::RpcConnection> RpcSession::assignServerToThisThread(unique_fd fd) {

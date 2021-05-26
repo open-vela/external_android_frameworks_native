1167165aab8a1c5ffb2886e8bab0109c6fe2f4a5
openvela-robot <openvela-robot@xiaomi.com>
2021-05-27 00:11:47 +0800

    Merge changes I5f58750a,Ifb74b312,I4eda926a
    * changes:
      binder_rpc_fuzzer: fuzz multiple connections
      libbinder: quiet RPC read/write failures
      libbinder: update RpcSession 'session' references

--- libs/binder/RpcSession.cpp
+++ libs/binder/RpcSession.cpp
@@ -219,7 +219,23 @@ void RpcSession::join(unique_fd client) {
     }
 
     if (server != nullptr) {
-        server->onSessionServerThreadEnded(sp<RpcSession>::fromExisting(this));
+        server->onSessionThreadEnding(sp<RpcSession>::fromExisting(this));
+    }
+}
+
+void RpcSession::terminateLocked() {
+    // TODO(b/185167543):
+    // - kindly notify other side of the connection of termination (can't be
+    // locked)
+    // - prevent new client/servers from being added
+    // - stop all threads which are currently reading/writing
+    // - terminate RpcState?
+
+    if (mTerminated) return;
+
+    sp<RpcServer> server = mForServer.promote();
+    if (server) {
+        server->onSessionTerminating(sp<RpcSession>::fromExisting(this));
     }
 }
 
@@ -343,10 +359,7 @@ bool RpcSession::removeServerConnection(const sp<RpcConnection>& connection) {
         it != mServerConnections.end()) {
         mServerConnections.erase(it);
         if (mServerConnections.size() == 0) {
-            sp<RpcServer> server = mForServer.promote();
-            if (server) {
-                server->onSessionLockedAllServerThreadsEnded(sp<RpcSession>::fromExisting(this));
-            }
+            terminateLocked();
         }
         return true;
     }

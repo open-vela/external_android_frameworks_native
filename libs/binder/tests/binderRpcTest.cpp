c0a1b9c1c1a49f9ae414b9038a578798019c9fbb
liujinye <liujinye@xiaomi.com>
2021-06-02 18:20:21 +0800

    Merge "libbinder: reverse connections" am: 50cc978efe am: c5dc8e0558 am: f7925c2065 am: 9eb25fc9b3
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1702014
    
    Change-Id: Iba15250f579fbca9eece80abdedb2523a6b514ad

--- libs/binder/tests/binderRpcTest.cpp
+++ libs/binder/tests/binderRpcTest.cpp
@@ -446,7 +446,7 @@ public:
 
         for (size_t i = 0; i < numSessions; i++) {
             sp<RpcSession> session = RpcSession::make();
-            session->setMaxThreads(numReverseConnections);
+            session->setMaxReverseConnections(numReverseConnections);
 
             switch (socketType) {
                 case SocketType::UNIX:

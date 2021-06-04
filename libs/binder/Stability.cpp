570464b3344d8ecf9725530339ed5c2196c8a35f
openvela-robot <openvela-robot@xiaomi.com>
2021-06-04 18:04:53 +0800

    Merge changes I5fe33278,I6aede64e,I541f7c3e am: 286347bfd7 am: 7ad9b075af am: c445c0550c am: 7794b7649a
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1727276
    
    Change-Id: I0f74769f57c434cc6a551d040bcf242950922372

--- libs/binder/Stability.cpp
+++ libs/binder/Stability.cpp
@@ -33,6 +33,7 @@ constexpr uint8_t kBinderWireFormatVersion = 1;
 Stability::Category Stability::Category::currentFromLevel(Level level) {
     return {
         .version = kBinderWireFormatVersion,
+        .reserved = {0},
         .level = level,
     };
 }

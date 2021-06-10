a809be17312e401c432eabe59b681747b8b65408
openvela-robot <openvela-robot@xiaomi.com>
2021-06-10 19:52:14 +0800

    Merge "binder: setRpcClientDebug copy # threads from kernel binder." am: aa19a50a02 am: 94e9b98e40
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1731911
    
    Change-Id: I9f753a7e34f3a2d433f7f7cbdf555eac82895f6b

--- libs/binder/RpcServer.cpp
+++ libs/binder/RpcServer.cpp
@@ -307,15 +307,13 @@ void RpcServer::establishConnection(sp<RpcServer>&& server, base::unique_fd clie
         }
 
         detachGuard.Disable();
-        session->preJoinThreadOwnership(std::move(thisThread));
+        session->preJoin(std::move(thisThread));
     }
-
-    auto setupResult = session->preJoinSetup(std::move(clientFd));
 
     // avoid strong cycle
     server = nullptr;
 
-    RpcSession::join(std::move(session), std::move(setupResult));
+    RpcSession::join(std::move(session), std::move(clientFd));
 }
 
 bool RpcServer::setupSocketServer(const RpcSocketAddress& addr) {

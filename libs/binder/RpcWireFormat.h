24327776066322b411ac7d2375d7de26aabfac55
openvela-robot <openvela-robot@xiaomi.com>
2021-06-15 18:15:41 +0800

    [automerger skipped] Merge "binder: rust: Fix dropped service memleak" am: f7bb463889 am: 2915402212 -s ours
    am skip reason: Merged-In Ie9932cc67016eecff4d24ac0c8fcd1bc7df27b32 with SHA-1 977461ff81 is already in history
    
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1735010
    
    Change-Id: I2ee60e93a1f1a29edc71bf033737d804f085c4bc

--- libs/binder/RpcWireFormat.h
+++ libs/binder/RpcWireFormat.h
@@ -20,6 +20,34 @@ namespace android {
 #pragma clang diagnostic push
 #pragma clang diagnostic error "-Wpadded"
 
+constexpr int32_t RPC_SESSION_ID_NEW = -1;
+
+enum : uint8_t {
+    RPC_CONNECTION_OPTION_REVERSE = 0x1,
+};
+
+/**
+ * This is sent to an RpcServer in order to request a new connection is created,
+ * either as part of a new session or an existing session
+ */
+struct RpcConnectionHeader {
+    int32_t sessionId;
+    uint8_t options;
+    uint8_t reserved[3];
+};
+
+#define RPC_CONNECTION_INIT_OKAY "cci"
+
+/**
+ * Whenever a client connection is setup, this is sent as the initial
+ * transaction. The main use of this is in order to control the timing for when
+ * a reverse connection is setup.
+ */
+struct RpcClientConnectionInit {
+    char msg[4];
+    uint8_t reserved[4];
+};
+
 enum : uint32_t {
     /**
      * follows is RpcWireTransaction, if flags != oneway, reply w/ RPC_COMMAND_REPLY expected
@@ -50,8 +78,6 @@ enum : uint32_t {
     RPC_SPECIAL_TRANSACT_GET_MAX_THREADS = 1,
     RPC_SPECIAL_TRANSACT_GET_SESSION_ID = 2,
 };
-
-constexpr int32_t RPC_SESSION_ID_NEW = -1;
 
 // serialization is like:
 // |RpcWireHeader|struct desginated by 'command'| (over and over again)

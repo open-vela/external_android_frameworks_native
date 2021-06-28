641933f445f8bcced9d65d7cb08c65650ac36e09
openvela-robot <openvela-robot@xiaomi.com>
2021-06-29 00:29:30 +0800

    Merge "libbinder: BBinder::attachObject abort on OOM" am: de610f7aba am: 92390fc176 am: f4518176b4
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1749082
    
    Change-Id: I46521c56f5ea53f65cbc2120fd797d59f5039d95

--- libs/binder/rust/src/proxy.rs
+++ libs/binder/rust/src/proxy.rs
@@ -48,13 +48,8 @@ impl fmt::Debug for SpIBinder {
 
 /// # Safety
 ///
-/// An `SpIBinder` is an immutable handle to a C++ IBinder, which is thread-safe
+/// An `SpIBinder` is a handle to a C++ IBinder, which is thread-safe
 unsafe impl Send for SpIBinder {}
-
-/// # Safety
-///
-/// An `SpIBinder` is an immutable handle to a C++ IBinder, which is thread-safe
-unsafe impl Sync for SpIBinder {}
 
 impl SpIBinder {
     /// Create an `SpIBinder` wrapper object from a raw `AIBinder` pointer.
@@ -453,13 +448,8 @@ impl fmt::Debug for WpIBinder {
 
 /// # Safety
 ///
-/// A `WpIBinder` is an immutable handle to a C++ IBinder, which is thread-safe.
+/// A `WpIBinder` is a handle to a C++ IBinder, which is thread-safe.
 unsafe impl Send for WpIBinder {}
-
-/// # Safety
-///
-/// A `WpIBinder` is an immutable handle to a C++ IBinder, which is thread-safe.
-unsafe impl Sync for WpIBinder {}
 
 impl WpIBinder {
     /// Create a new weak reference from an object that can be converted into a

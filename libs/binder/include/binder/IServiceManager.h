91aa5c09cf9c5e3251cb8640c1054d08cc1cc00a
openvela-robot <openvela-robot@xiaomi.com>
2021-07-28 20:44:31 +0800

    Merge "libbinder: RPC binder - incl. protocol version" am: ab24b86d32 am: a874239ddd am: 35573645a1 am: 6121dcd9d6
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1778952
    
    Change-Id: I95a8516d4958c7d2024052a2a690e805c70e5ae7

--- libs/binder/include/binder/IServiceManager.h
+++ libs/binder/include/binder/IServiceManager.h
@@ -167,8 +167,7 @@ status_t getService(const String16& name, sp<INTERFACE>* outService)
 bool checkCallingPermission(const String16& permission);
 bool checkCallingPermission(const String16& permission,
                             int32_t* outPid, int32_t* outUid);
-bool checkPermission(const String16& permission, pid_t pid, uid_t uid,
-                     bool logPermissionFailure = true);
+bool checkPermission(const String16& permission, pid_t pid, uid_t uid);
 
 #ifndef __ANDROID__
 // Create an IServiceManager that delegates the service manager on the device via adb.

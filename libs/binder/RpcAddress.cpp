1f97240f84b35a541fe7c5ed6f7b6c96d2d9bcb0
openvela-robot <openvela-robot@xiaomi.com>
2021-08-13 21:20:29 +0800

    [automerger skipped] Merge stage-aosp-sc-ts-dev before making it an upstream am: f4fc518bc6 -s ours am: 32ca6e8190 -s ours
    am skip reason: Merged-In I2a2d02a0d7e67ee9577857a210cb6157683e5598 with SHA-1 52846cd871 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/15550161
    
    Change-Id: I7bc8d9f2cac71fe07581ad25f922ca1de024c3c9

--- libs/binder/RpcAddress.cpp
+++ libs/binder/RpcAddress.cpp
@@ -16,7 +16,6 @@
 
 #include <binder/RpcAddress.h>
 
-#include <android-base/hex.h>
 #include <binder/Parcel.h>
 
 #include "Debug.h"
@@ -30,7 +29,7 @@ RpcAddress RpcAddress::zero() {
 }
 
 bool RpcAddress::isZero() const {
-    RpcWireAddress ZERO{.options = 0};
+    RpcWireAddress ZERO{0};
     return memcmp(mRawAddr.get(), &ZERO, sizeof(RpcWireAddress)) == 0;
 }
 
@@ -52,32 +51,11 @@ static void ReadRandomBytes(uint8_t* buf, size_t len) {
     close(fd);
 }
 
-RpcAddress RpcAddress::random(bool forServer) {
-    // The remainder of this header acts as reserved space for different kinds
-    // of binder objects.
-    uint64_t options = RPC_WIRE_ADDRESS_OPTION_CREATED;
-
-    // servers and clients allocate addresses independently, so this bit can
-    // tell you where an address originates
-    if (forServer) options |= RPC_WIRE_ADDRESS_OPTION_FOR_SERVER;
-
+RpcAddress RpcAddress::unique() {
     RpcAddress ret;
-    RpcWireAddress* raw = ret.mRawAddr.get();
-
-    raw->options = options;
-    ReadRandomBytes(raw->address, sizeof(raw->address));
-
+    ReadRandomBytes((uint8_t*)ret.mRawAddr.get(), sizeof(RpcWireAddress));
     LOG_RPC_DETAIL("Creating new address: %s", ret.toString().c_str());
     return ret;
-}
-
-bool RpcAddress::isForServer() const {
-    return mRawAddr.get()->options & RPC_WIRE_ADDRESS_OPTION_FOR_SERVER;
-}
-
-bool RpcAddress::isRecognizedType() const {
-    uint64_t allKnownOptions = RPC_WIRE_ADDRESS_OPTION_CREATED | RPC_WIRE_ADDRESS_OPTION_FOR_SERVER;
-    return (mRawAddr.get()->options & ~allKnownOptions) == 0;
 }
 
 RpcAddress RpcAddress::fromRawEmbedded(const RpcWireAddress* raw) {
@@ -95,7 +73,7 @@ bool RpcAddress::operator<(const RpcAddress& rhs) const {
 }
 
 std::string RpcAddress::toString() const {
-    return base::HexString(mRawAddr.get(), sizeof(RpcWireAddress));
+    return hexString(mRawAddr.get(), sizeof(RpcWireAddress));
 }
 
 status_t RpcAddress::writeToParcel(Parcel* parcel) const {

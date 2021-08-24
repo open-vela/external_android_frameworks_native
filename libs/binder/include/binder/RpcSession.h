d3779188c8feb18991ae1bfda77479e1b24078bd
openvela-robot <openvela-robot@xiaomi.com>
2021-08-24 23:50:21 +0800

    Merge "Extend rpc visibility to more virt packages" am: b329eca2cc
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1799567
    
    Change-Id: I9dfc68321195d272172d9c4f77ffa58a6551a943

--- libs/binder/include/binder/RpcSession.h
+++ libs/binder/include/binder/RpcSession.h
@@ -51,15 +51,8 @@ constexpr uint32_t RPC_WIRE_PROTOCOL_VERSION = RPC_WIRE_PROTOCOL_VERSION_EXPERIM
  */
 class RpcSession final : public virtual RefBase {
 public:
-    // Create an RpcSession with default configuration (raw sockets).
-    static sp<RpcSession> make();
-
-    // Create an RpcSession with the given configuration. |serverCertificateFormat| and
-    // |serverCertificate| must have values or be nullopt simultaneously. If they have values, set
-    // server certificate.
-    static sp<RpcSession> make(std::unique_ptr<RpcTransportCtxFactory> rpcTransportCtxFactory,
-                               std::optional<CertificateFormat> serverCertificateFormat,
-                               std::optional<std::string> serverCertificate);
+    static sp<RpcSession> make(
+            std::unique_ptr<RpcTransportCtxFactory> rpcTransportCtxFactory = nullptr);
 
     /**
      * Set the maximum number of threads allowed to be made (for things like callbacks).
@@ -132,11 +125,6 @@ public:
     status_t getRemoteMaxThreads(size_t* maxThreads);
 
     /**
-     * See RpcTransportCtx::getCertificate
-     */
-    std::string getCertificate(CertificateFormat);
-
-    /**
      * Shuts down the service.
      *
      * For client sessions, wait can be true or false. For server sessions,
@@ -171,7 +159,7 @@ private:
     friend sp<RpcSession>;
     friend RpcServer;
     friend RpcState;
-    explicit RpcSession(std::unique_ptr<RpcTransportCtx> ctx);
+    explicit RpcSession(std::unique_ptr<RpcTransportCtxFactory> rpcTransportCtxFactory);
 
     class EventListener : public virtual RefBase {
     public:
@@ -271,7 +259,7 @@ private:
         bool mReentrant = false;
     };
 
-    const std::unique_ptr<RpcTransportCtx> mCtx;
+    const std::unique_ptr<RpcTransportCtxFactory> mRpcTransportCtxFactory;
 
     // On the other side of a session, for each of mOutgoingConnections here, there should
     // be one of mIncomingConnections on the other side (and vice versa).

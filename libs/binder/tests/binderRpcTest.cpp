7fe29ad69ce1609738121f5c519047f6a02361cc
openvela-robot <openvela-robot@xiaomi.com>
2021-08-27 10:14:10 +0800

    [automerger skipped] Merge "Remove ndk_platform backend. Use the ndk backend." am: 3d7fccae18 -s ours am: 3c41441c57 -s ours am: 104b1b15ff -s ours am: d9f735df88 -s ours
    am skip reason: Merged-In I1e84945fcfef3b2a300811f27ef57059eaf08553 with SHA-1 8db75928a8 is already in history
    
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1787977
    
    Change-Id: I4492539ec64843f4ef2ca3b6e31891971ec51d4e

--- libs/binder/tests/binderRpcTest.cpp
+++ libs/binder/tests/binderRpcTest.cpp
@@ -31,7 +31,6 @@
 #include <binder/RpcSession.h>
 #include <binder/RpcTransport.h>
 #include <binder/RpcTransportRaw.h>
-#include <binder/RpcTransportTls.h>
 #include <gtest/gtest.h>
 
 #include <chrono>
@@ -55,18 +54,16 @@ static_assert(RPC_WIRE_PROTOCOL_VERSION + 1 == RPC_WIRE_PROTOCOL_VERSION_NEXT ||
               RPC_WIRE_PROTOCOL_VERSION == RPC_WIRE_PROTOCOL_VERSION_EXPERIMENTAL);
 const char* kLocalInetAddress = "127.0.0.1";
 
-enum class RpcSecurity { RAW, TLS };
+enum class RpcSecurity { RAW };
 
 static inline std::vector<RpcSecurity> RpcSecurityValues() {
-    return {RpcSecurity::RAW, RpcSecurity::TLS};
+    return {RpcSecurity::RAW};
 }
 
 static inline std::unique_ptr<RpcTransportCtxFactory> newFactory(RpcSecurity rpcSecurity) {
     switch (rpcSecurity) {
         case RpcSecurity::RAW:
             return RpcTransportCtxFactoryRaw::make();
-        case RpcSecurity::TLS:
-            return RpcTransportCtxFactoryTls::make();
         default:
             LOG_ALWAYS_FATAL("Unknown RpcSecurity %d", rpcSecurity);
     }

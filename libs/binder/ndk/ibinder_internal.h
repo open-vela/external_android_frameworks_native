9278e4f3fc8f17c3996c414fb134b3e1aed53075
openvela-robot <openvela-robot@xiaomi.com>
2021-09-03 22:39:10 +0800

    libbinder: uptimeMillis returns int64_t! am: 2923788f33 am: 9712c71562 am: 8a09003ef0
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/15720346
    
    Bug: 197336441
    
    Change-Id: I703760ecbfa007d27caf76556ed21bbf0558df80

--- libs/binder/ndk/ibinder_internal.h
+++ libs/binder/ndk/ibinder_internal.h
@@ -17,6 +17,7 @@
 #pragma once
 
 #include <android/binder_ibinder.h>
+#include <android/binder_shell.h>
 #include "ibinder_internal.h"
 
 #include <atomic>
@@ -115,6 +116,7 @@ struct AIBinder_Class {
 
     // optional methods for a class
     AIBinder_onDump onDump = nullptr;
+    AIBinder_handleShellCommand handleShellCommand = nullptr;
 
    private:
     // This must be a String16 since BBinder virtual getInterfaceDescriptor returns a reference to

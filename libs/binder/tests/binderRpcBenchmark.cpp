37c0d94a62f6e144f013363136ab5e5810a293b5
openvela-robot <openvela-robot@xiaomi.com>
2021-09-07 23:38:48 +0800

    [automerger skipped] libbinder: uptimeMillis returns int64_t! am: 2923788f33 am: 9712c71562 am: 8a09003ef0 am: 32058a3783 am: a2f8583317 -s ours am: 7c079cf6bd -s ours am: a4eac5db0b -s ours
    am skip reason: skipped by user smoreland
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/15759163
    
    Change-Id: I832a391082748ae59b138902ae487cf2f739c603

--- libs/binder/tests/binderRpcBenchmark.cpp
+++ libs/binder/tests/binderRpcBenchmark.cpp
@@ -18,33 +18,21 @@
 #include <android-base/logging.h>
 #include <benchmark/benchmark.h>
 #include <binder/Binder.h>
-#include <binder/IPCThreadState.h>
-#include <binder/IServiceManager.h>
-#include <binder/ProcessState.h>
 #include <binder/RpcServer.h>
 #include <binder/RpcSession.h>
 
 #include <thread>
 
-#include <signal.h>
-#include <sys/prctl.h>
 #include <sys/types.h>
 #include <unistd.h>
 
 using android::BBinder;
-using android::defaultServiceManager;
 using android::IBinder;
 using android::interface_cast;
-using android::IPCThreadState;
-using android::IServiceManager;
 using android::OK;
-using android::ProcessState;
 using android::RpcServer;
 using android::RpcSession;
 using android::sp;
-using android::status_t;
-using android::statusToString;
-using android::String16;
 using android::binder::Status;
 
 class MyBinderRpcBenchmark : public BnBinderRpcBenchmark {
@@ -52,60 +40,34 @@ class MyBinderRpcBenchmark : public BnBinderRpcBenchmark {
         *out = str;
         return Status::ok();
     }
-    Status repeatBinder(const sp<IBinder>& binder, sp<IBinder>* out) override {
-        *out = binder;
-        return Status::ok();
-    }
-    Status repeatBytes(const std::vector<uint8_t>& bytes, std::vector<uint8_t>* out) override {
-        *out = bytes;
+    Status repeatBinder(const sp<IBinder>& str, sp<IBinder>* out) override {
+        *out = str;
         return Status::ok();
     }
 };
-
-enum Transport {
-    KERNEL,
-    RPC,
-};
-
-static const std::initializer_list<int64_t> kTransportList = {
-#ifdef __BIONIC__
-        Transport::KERNEL,
-#endif
-        Transport::RPC};
 
 static sp<RpcSession> gSession = RpcSession::make();
-#ifdef __BIONIC__
-static const String16 kKernelBinderInstance = String16(u"binderRpcBenchmark-control");
-static sp<IBinder> gKernelBinder;
-#endif
 
-static sp<IBinder> getBinderForOptions(benchmark::State& state) {
-    Transport transport = static_cast<Transport>(state.range(0));
-    switch (transport) {
-#ifdef __BIONIC__
-        case KERNEL:
-            return gKernelBinder;
-#endif
-        case RPC:
-            return gSession->getRootObject();
-        default:
-            LOG(FATAL) << "Unknown transport value: " << transport;
-            return nullptr;
+void BM_getRootObject(benchmark::State& state) {
+    while (state.KeepRunning()) {
+        CHECK(gSession->getRootObject() != nullptr);
     }
 }
+BENCHMARK(BM_getRootObject);
 
 void BM_pingTransaction(benchmark::State& state) {
-    sp<IBinder> binder = getBinderForOptions(state);
+    sp<IBinder> binder = gSession->getRootObject();
+    CHECK(binder != nullptr);
 
     while (state.KeepRunning()) {
         CHECK_EQ(OK, binder->pingBinder());
     }
 }
-BENCHMARK(BM_pingTransaction)->ArgsProduct({kTransportList});
+BENCHMARK(BM_pingTransaction);
 
-void BM_repeatTwoPageString(benchmark::State& state) {
-    sp<IBinder> binder = getBinderForOptions(state);
-
+void BM_repeatString(benchmark::State& state) {
+    sp<IBinder> binder = gSession->getRootObject();
+    CHECK(binder != nullptr);
     sp<IBinderRpcBenchmark> iface = interface_cast<IBinderRpcBenchmark>(binder);
     CHECK(iface != nullptr);
 
@@ -130,30 +92,10 @@ void BM_repeatTwoPageString(benchmark::State& state) {
         CHECK(ret.isOk()) << ret;
     }
 }
-BENCHMARK(BM_repeatTwoPageString)->ArgsProduct({kTransportList});
-
-void BM_throughputForTransportAndBytes(benchmark::State& state) {
-    sp<IBinder> binder = getBinderForOptions(state);
-    sp<IBinderRpcBenchmark> iface = interface_cast<IBinderRpcBenchmark>(binder);
-    CHECK(iface != nullptr);
-
-    std::vector<uint8_t> bytes = std::vector<uint8_t>(state.range(1));
-    for (size_t i = 0; i < bytes.size(); i++) {
-        bytes[i] = i % 256;
-    }
-
-    while (state.KeepRunning()) {
-        std::vector<uint8_t> out;
-        Status ret = iface->repeatBytes(bytes, &out);
-        CHECK(ret.isOk()) << ret;
-    }
-}
-BENCHMARK(BM_throughputForTransportAndBytes)
-        ->ArgsProduct({kTransportList,
-                       {64, 1024, 2048, 4096, 8182, 16364, 32728, 65535, 65536, 65537}});
+BENCHMARK(BM_repeatString);
 
 void BM_repeatBinder(benchmark::State& state) {
-    sp<IBinder> binder = getBinderForOptions(state);
+    sp<IBinder> binder = gSession->getRootObject();
     CHECK(binder != nullptr);
     sp<IBinderRpcBenchmark> iface = interface_cast<IBinderRpcBenchmark>(binder);
     CHECK(iface != nullptr);
@@ -167,7 +109,7 @@ void BM_repeatBinder(benchmark::State& state) {
         CHECK(ret.isOk()) << ret;
     }
 }
-BENCHMARK(BM_repeatBinder)->ArgsProduct({kTransportList});
+BENCHMARK(BM_repeatBinder);
 
 int main(int argc, char** argv) {
     ::benchmark::Initialize(&argc, argv);
@@ -176,44 +118,19 @@ int main(int argc, char** argv) {
     std::string addr = std::string(getenv("TMPDIR") ?: "/tmp") + "/binderRpcBenchmark";
     (void)unlink(addr.c_str());
 
-    std::cerr << "Tests suffixes:" << std::endl;
-    std::cerr << "\t.../" << Transport::KERNEL << " is KERNEL" << std::endl;
-    std::cerr << "\t.../" << Transport::RPC << " is RPC" << std::endl;
-
-    if (0 == fork()) {
-        prctl(PR_SET_PDEATHSIG, SIGHUP); // racey, okay
+    std::thread([addr]() {
         sp<RpcServer> server = RpcServer::make();
         server->setRootObject(sp<MyBinderRpcBenchmark>::make());
         server->iUnderstandThisCodeIsExperimentalAndIWillNotUseItInProduction();
-        CHECK_EQ(OK, server->setupUnixDomainServer(addr.c_str()));
+        CHECK(server->setupUnixDomainServer(addr.c_str()));
         server->join();
-        exit(1);
-    }
+    }).detach();
 
-#ifdef __BIONIC__
-    if (0 == fork()) {
-        prctl(PR_SET_PDEATHSIG, SIGHUP); // racey, okay
-        CHECK_EQ(OK,
-                 defaultServiceManager()->addService(kKernelBinderInstance,
-                                                     sp<MyBinderRpcBenchmark>::make()));
-        IPCThreadState::self()->joinThreadPool();
-        exit(1);
-    }
-
-    ProcessState::self()->setThreadPoolMaxThreadCount(1);
-    ProcessState::self()->startThreadPool();
-
-    gKernelBinder = defaultServiceManager()->waitForService(kKernelBinderInstance);
-    CHECK_NE(nullptr, gKernelBinder.get());
-#endif
-
-    status_t status;
     for (size_t tries = 0; tries < 5; tries++) {
         usleep(10000);
-        status = gSession->setupUnixDomainClient(addr.c_str());
-        if (status == OK) goto success;
+        if (gSession->setupUnixDomainClient(addr.c_str())) goto success;
     }
-    LOG(FATAL) << "Could not connect: " << statusToString(status).c_str();
+    LOG(FATAL) << "Could not connect.";
 success:
 
     ::benchmark::RunSpecifiedBenchmarks();

d74584dd33005d23c2149c0ea41e91ddc302e598
openvela-robot <openvela-robot@xiaomi.com>
2021-09-15 14:55:47 +0800

    [automerger skipped] Merge "getProcessFreezeInfo reads more info from kernel" into sc-qpr1-dev am: 4430ebe5aa -s ours
    am skip reason: Merged-In Ia7ce6899ddaeb8e8453b0b8f3536d129b6b761d3 with SHA-1 24494091d3 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/15793536
    
    Change-Id: I867cb5a0b33ab566674fad268085547dd9b4e96b

--- libs/binder/ndk/ibinder_internal.h
+++ libs/binder/ndk/ibinder_internal.h
@@ -120,13 +120,13 @@ struct AIBinder_Class {
     bool writeHeader = true;
 
     // required to be non-null, implemented for every class
-    const AIBinder_Class_onCreate onCreate;
-    const AIBinder_Class_onDestroy onDestroy;
-    const AIBinder_Class_onTransact onTransact;
+    const AIBinder_Class_onCreate onCreate = nullptr;
+    const AIBinder_Class_onDestroy onDestroy = nullptr;
+    const AIBinder_Class_onTransact onTransact = nullptr;
 
     // optional methods for a class
-    AIBinder_onDump onDump;
-    AIBinder_handleShellCommand handleShellCommand;
+    AIBinder_onDump onDump = nullptr;
+    AIBinder_handleShellCommand handleShellCommand = nullptr;
 
    private:
     // Copy of the raw char string for when we don't have to return UTF-16
@@ -148,14 +148,8 @@ struct AIBinder_DeathRecipient : ::android::RefBase {
     struct TransferDeathRecipient : ::android::IBinder::DeathRecipient {
         TransferDeathRecipient(const ::android::wp<::android::IBinder>& who, void* cookie,
                                const ::android::wp<AIBinder_DeathRecipient>& parentRecipient,
-                               const AIBinder_DeathRecipient_onBinderDied onDied,
-                               const AIBinder_DeathRecipient_onBinderUnlinked onUnlinked)
-            : mWho(who),
-              mCookie(cookie),
-              mParentRecipient(parentRecipient),
-              mOnDied(onDied),
-              mOnUnlinked(onUnlinked) {}
-        ~TransferDeathRecipient();
+                               const AIBinder_DeathRecipient_onBinderDied onDied)
+            : mWho(who), mCookie(cookie), mParentRecipient(parentRecipient), mOnDied(onDied) {}
 
         void binderDied(const ::android::wp<::android::IBinder>& who) override;
 
@@ -171,13 +165,11 @@ struct AIBinder_DeathRecipient : ::android::RefBase {
         // This is kept separately from AIBinder_DeathRecipient in case the death recipient is
         // deleted while the death notification is fired
         const AIBinder_DeathRecipient_onBinderDied mOnDied;
-        const AIBinder_DeathRecipient_onBinderUnlinked mOnUnlinked;
     };
 
     explicit AIBinder_DeathRecipient(AIBinder_DeathRecipient_onBinderDied onDied);
     binder_status_t linkToDeath(const ::android::sp<::android::IBinder>&, void* cookie);
     binder_status_t unlinkToDeath(const ::android::sp<::android::IBinder>& binder, void* cookie);
-    void setOnUnlinked(AIBinder_DeathRecipient_onBinderUnlinked onUnlinked);
 
    private:
     // When the user of this API deletes a Bp object but not the death recipient, the
@@ -188,5 +180,4 @@ struct AIBinder_DeathRecipient : ::android::RefBase {
     std::mutex mDeathRecipientsMutex;
     std::vector<::android::sp<TransferDeathRecipient>> mDeathRecipients;
     AIBinder_DeathRecipient_onBinderDied mOnDied;
-    AIBinder_DeathRecipient_onBinderUnlinked mOnUnlinked;
 };

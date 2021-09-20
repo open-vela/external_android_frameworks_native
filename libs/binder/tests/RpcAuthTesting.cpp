8ad2dae6e63ee31851a135af2b2cc11cc46ac460
liujinye <liujinye@xiaomi.com>
2021-09-21 00:26:19 +0800

    Merge "binder_rpc_fuzzer: use ConsumeRandomLengthString"

--- libs/binder/tests/RpcAuthTesting.cpp
+++ libs/binder/tests/RpcAuthTesting.cpp
@@ -70,14 +70,4 @@ status_t RpcAuthSelfSigned::configure(SSL_CTX* ctx) {
     return OK;
 }
 
-status_t RpcAuthPreSigned::configure(SSL_CTX* ctx) {
-    if (!SSL_CTX_use_PrivateKey(ctx, mPkey.get())) {
-        return INVALID_OPERATION;
-    }
-    if (!SSL_CTX_use_certificate(ctx, mCert.get())) {
-        return INVALID_OPERATION;
-    }
-    return OK;
-}
-
 } // namespace android

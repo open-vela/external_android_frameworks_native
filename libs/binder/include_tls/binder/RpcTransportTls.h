39a33999f1caa7ca08d91f3af0d48370092f5b09
liujinye <liujinye@xiaomi.com>
2021-09-21 00:50:56 +0800

    Merge "binderRpcTest: fix OnewayStressTest flake" am: a43825ddd0 am: db0df0cb2d am: a52e41b019 am: bf315edd5e am: 176f2981c5
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1830385
    
    Change-Id: I2e1890c46b60cd3c5eb6b1aef594de1cba20c9dc

--- libs/binder/include_tls/binder/RpcTransportTls.h
+++ libs/binder/include_tls/binder/RpcTransportTls.h
@@ -18,7 +18,6 @@
 
 #pragma once
 
-#include <binder/RpcAuth.h>
 #include <binder/RpcCertificateVerifier.h>
 #include <binder/RpcTransport.h>
 
@@ -27,20 +26,17 @@ namespace android {
 // RpcTransportCtxFactory with TLS enabled with self-signed certificate.
 class RpcTransportCtxFactoryTls : public RpcTransportCtxFactory {
 public:
-    static std::unique_ptr<RpcTransportCtxFactory> make(std::shared_ptr<RpcCertificateVerifier>,
-                                                        std::unique_ptr<RpcAuth>);
+    static std::unique_ptr<RpcTransportCtxFactory> make(std::shared_ptr<RpcCertificateVerifier>);
 
     std::unique_ptr<RpcTransportCtx> newServerCtx() const override;
     std::unique_ptr<RpcTransportCtx> newClientCtx() const override;
     const char* toCString() const override;
 
 private:
-    RpcTransportCtxFactoryTls(std::shared_ptr<RpcCertificateVerifier> verifier,
-                              std::unique_ptr<RpcAuth> auth)
-          : mCertVerifier(std::move(verifier)), mAuth(std::move(auth)){};
+    RpcTransportCtxFactoryTls(std::shared_ptr<RpcCertificateVerifier> verifier)
+          : mCertVerifier(std::move(verifier)){};
 
     std::shared_ptr<RpcCertificateVerifier> mCertVerifier;
-    std::unique_ptr<RpcAuth> mAuth;
 };
 
 } // namespace android

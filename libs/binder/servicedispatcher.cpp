234d18f06e10b59ca9873a7b217f6da5a545a4c0
liujinye <liujinye@xiaomi.com>
2021-09-23 05:32:01 +0800

    [automerger skipped] Merge "libbinder: split out PackageManagerNative aidl" into stage-aosp-master am: 9c78de8387 am: c881c2df23 am: 79c5cb57eb -s ours
    am skip reason: Merged-In Ic2562a3ba1d7d84ab6c9a3836976608a3aa0b2dd with SHA-1 51b45573c0 is already in history
    
    Original change: https://googleplex-android-review.googlesource.com/c/platform/frameworks/native/+/15836028
    
    Change-Id: I5ac114199de0b1d27857fe9a0849ced368ea39ca

--- libs/binder/servicedispatcher.cpp
+++ libs/binder/servicedispatcher.cpp
@@ -157,11 +157,6 @@ public:
                                              std::optional<std::string>* _aidl_return) override {
         return mImpl->updatableViaApex(name, _aidl_return);
     }
-    android::binder::Status getConnectionInfo(
-            const std::string& name,
-            std::optional<android::os::ConnectionInfo>* _aidl_return) override {
-        return mImpl->getConnectionInfo(name, _aidl_return);
-    }
     android::binder::Status registerClientCallback(
             const std::string&, const android::sp<android::IBinder>&,
             const android::sp<android::os::IClientCallback>&) override {

5e94b23ed72280334ba17b53a466c6d2b2a8c2bf
liujinye <liujinye@xiaomi.com>
2021-10-08 01:35:00 +0800

    getStagedApexInfo() returns @nullable am: b922605762
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/1844834
    
    Change-Id: I7b28ed4acf3ef4296ce6a9f3307888018dd663c3

--- libs/binder/include/binder/Parcel.h
+++ libs/binder/include/binder/Parcel.h
@@ -1141,7 +1141,6 @@ private:
     release_func        mOwner;
 
     sp<RpcSession> mSession;
-    size_t mReserved;
 
     class Blob {
     public:
@@ -1226,17 +1225,13 @@ public:
         inline void* data() { return mData; }
     };
 
-    /**
-     * Returns the total amount of ashmem memory owned by this object.
-     *
-     * Note: for historical reasons, this does not include ashmem memory which
-     * is referenced by this Parcel, but which this parcel doesn't own (e.g.
-     * writeFileDescriptor is called without 'takeOwnership' true).
-     */
-    size_t getOpenAshmemSize() const;
+private:
+    size_t mOpenAshmemSize;
 
-    // TODO(b/202029388): Remove 'getBlobAshmemSize' once ABI can be changed.
+public:
+    // TODO: Remove once ABI can be changed.
     size_t getBlobAshmemSize() const;
+    size_t getOpenAshmemSize() const;
 };
 
 // ---------------------------------------------------------------------------

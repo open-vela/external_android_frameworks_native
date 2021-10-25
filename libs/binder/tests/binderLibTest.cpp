0d4a362abb36a055c93859cc3df16a2d5b47f56e
liujinye <liujinye@xiaomi.com>
2021-10-25 17:06:07 +0800

    Merge "Replace writepid with task_profiles command for cgroup migration"

--- libs/binder/tests/binderLibTest.cpp
+++ libs/binder/tests/binderLibTest.cpp
@@ -496,7 +496,7 @@ TEST_F(BinderLibTest, Freeze) {
     EXPECT_EQ(NO_ERROR, IPCThreadState::self()->freeze(pid, true, 1000));
     EXPECT_EQ(FAILED_TRANSACTION, m_server->transact(BINDER_LIB_TEST_NOP_TRANSACTION, data, &reply));
 
-    uint32_t sync_received, async_received;
+    bool sync_received, async_received;
 
     EXPECT_EQ(NO_ERROR, IPCThreadState::self()->getProcessFreezeInfo(pid, &sync_received,
                 &async_received));
@@ -504,7 +504,15 @@ TEST_F(BinderLibTest, Freeze) {
     EXPECT_EQ(sync_received, 1);
     EXPECT_EQ(async_received, 0);
 
-    EXPECT_EQ(NO_ERROR, IPCThreadState::self()->freeze(pid, false, 0));
+    uint32_t sync_received2, async_received2;
+
+    EXPECT_EQ(NO_ERROR, IPCThreadState::self()->getProcessFreezeInfo(pid, &sync_received2,
+                &async_received2));
+
+    EXPECT_EQ(sync_received2, 1);
+    EXPECT_EQ(async_received2, 0);
+
+    EXPECT_EQ(NO_ERROR, IPCThreadState::self()->freeze(pid, 0, 0));
     EXPECT_EQ(NO_ERROR, m_server->transact(BINDER_LIB_TEST_NOP_TRANSACTION, data, &reply));
 }
 
@@ -1194,6 +1202,7 @@ public:
         auto rpcServer = RpcServer::make();
         EXPECT_NE(nullptr, rpcServer);
         if (rpcServer == nullptr) return {};
+        rpcServer->iUnderstandThisCodeIsExperimentalAndIWillNotUseItInProduction();
         unsigned int port;
         if (status_t status = rpcServer->setupInetServer("127.0.0.1", 0, &port); status != OK) {
             ADD_FAILURE() << "setupInetServer failed" << statusToString(status);

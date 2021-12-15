e32f20d27177cbbdfe52d34ff97f02a93714c9ac
openvela-robot <openvela-robot@xiaomi.com>
2021-12-16 05:23:19 +0800

    Merge changes from topic "fixed-size-array-rust"
    * changes:
      libbinder_rs: Support fixed-size arrays
      libbinder_rs: PartialEq for ParcelFileDescriptor

--- libs/binder/ProcessState.cpp
+++ libs/binder/ProcessState.cpp
@@ -176,11 +176,6 @@ void ProcessState::startThreadPool()
 {
     AutoMutex _l(mLock);
     if (!mThreadPoolStarted) {
-        if (mMaxThreads == 0) {
-            ALOGW("Extra binder thread started, but 0 threads requested. Do not use "
-                  "*startThreadPool when zero threads are requested.");
-        }
-
         mThreadPoolStarted = true;
         spawnPooledThread(true);
     }

df15092533fb78088cfce9e342795da53bb85a4d
openvela-robot <openvela-robot@xiaomi.com>
2022-03-02 19:42:13 +0800

    Merge "Changes for the Rust 1.59.0 update" am: 8a4e1aa561
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/2000141
    
    Change-Id: Ic5b00b0a9c612b985d8ba9f63446996d0a2018bc

--- libs/binder/Parcel.cpp
+++ libs/binder/Parcel.cpp
@@ -200,6 +200,7 @@ status_t Parcel::flattenBinder(const sp<IBinder>& binder) {
     }
 
     flat_binder_object obj;
+    obj.flags = FLAT_BINDER_FLAG_ACCEPTS_FDS;
 
     int schedBits = 0;
     if (!IPCThreadState::self()->backgroundSchedulingDisabled()) {
@@ -220,7 +221,6 @@ status_t Parcel::flattenBinder(const sp<IBinder>& binder) {
             const int32_t handle = proxy ? proxy->getPrivateAccessor().binderHandle() : 0;
             obj.hdr.type = BINDER_TYPE_HANDLE;
             obj.binder = 0; /* Don't pass uninitialized stack data to a remote process */
-            obj.flags = 0;
             obj.handle = handle;
             obj.cookie = 0;
         } else {
@@ -231,7 +231,6 @@ status_t Parcel::flattenBinder(const sp<IBinder>& binder) {
                 // override value, since it is set explicitly
                 schedBits = schedPolicyMask(policy, priority);
             }
-            obj.flags = FLAT_BINDER_FLAG_ACCEPTS_FDS;
             if (local->isRequestingSid()) {
                 obj.flags |= FLAT_BINDER_FLAG_TXN_SECURITY_CTX;
             }
@@ -244,7 +243,6 @@ status_t Parcel::flattenBinder(const sp<IBinder>& binder) {
         }
     } else {
         obj.hdr.type = BINDER_TYPE_BINDER;
-        obj.flags = 0;
         obj.binder = 0;
         obj.cookie = 0;
     }

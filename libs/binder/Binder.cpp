2bfbaf08e3f4c4bf68a7fa04e6b29d68718b15b7
liujinye <liujinye@xiaomi.com>
2022-03-09 11:23:20 +0800

    Merge "libbinder_ndk: conditional __builtin_available" am: 499ac942b2 am: 8a45f51230 am: 7767bde050
    Original change: https://android-review.googlesource.com/c/platform/frameworks/native/+/2011880
    
    Change-Id: I048ee5714682e96d21fd0f5fee2c08371b34e2c4

--- libs/binder/Binder.cpp
+++ libs/binder/Binder.cpp
@@ -19,7 +19,6 @@
 #include <atomic>
 #include <set>
 
-#include <android-base/logging.h>
 #include <android-base/unique_fd.h>
 #include <binder/BpBinder.h>
 #include <binder/IInterface.h>
@@ -282,11 +281,9 @@ status_t BBinder::transact(
             err = pingBinder();
             break;
         case EXTENSION_TRANSACTION:
-            CHECK(reply != nullptr);
             err = reply->writeStrongBinder(getExtension());
             break;
         case DEBUG_PID_TRANSACTION:
-            CHECK(reply != nullptr);
             err = reply->writeInt32(getDebugPid());
             break;
         case SET_RPC_CLIENT_TRANSACTION: {
@@ -593,7 +590,6 @@ status_t BBinder::onTransact(
 {
     switch (code) {
         case INTERFACE_TRANSACTION:
-            CHECK(reply != nullptr);
             reply->writeString16(getInterfaceDescriptor());
             return NO_ERROR;
 

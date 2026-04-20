littleGnAl/mirror_platform_frameworks_native

libbinder: __ANDROID_HOST__ => !__ANDROID__
Slightly simplifies config and probably makes more sense.

Bug: 124524556
Test: mma (continues to build on host)
Change-Id: Id11dbc25b581919ce8814c9c9bca191071434142

libs/binder/include/private/binder/binder_module.h
@@ -24,7 +24,7 @@ namespace android {
 /* obtain structures and constants from the kernel header */
 
 // TODO(b/31559095): bionic on host
-#ifdef __ANDROID_HOST__
+#ifndef __ANDROD__
 #define __packed __attribute__((__packed__))
 #endif
 

open-vela/external_android_frameworks_native

libs/binder/include/private/binder/binder_module.h
@@ -24,7 +24,7 @@ namespace android {
 /* obtain structures and constants from the kernel header */
 
 // TODO(b/31559095): bionic on host
-#ifdef __ANDROID_HOST__
+#ifndef __ANDROID__
 #define __packed __attribute__((__packed__))
 #endif
 